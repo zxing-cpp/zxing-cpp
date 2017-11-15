2b6301c56bcbb674ef0e3870725a6cbf49c62880
Axel Waggershauser <awagger@gmail.com>
2017-11-15 23:53:10 +0100

    Fix a porting bug in DMHighLevelEncoder
    Fix a bug that could cause the 3rd last character in a string to get
    lost when TEXT encoding is used. The std::function<> replacement of the
    JAVA virtual function call was missing one instance that led to the
    call of the C40::EncodeChar instead of Text::EncodeChar.
    
    That was a rather tricky one ;). Sticking with a runtime polymorphism
    class hierarchy might have been the better choice here.

--- core/src/datamatrix/DMHighLevelEncoder.cpp
+++ core/src/datamatrix/DMHighLevelEncoder.cpp
@@ -422,12 +422,12 @@ namespace C40Encoder {
 		throw std::invalid_argument("Illegal character: " + ToHexString(c));
 	}
 
-	static int BacktrackOneCharacter(EncoderContext& context, std::string& buffer, std::string& removed, int lastCharSize)
+	static int BacktrackOneCharacter(EncoderContext& context, std::string& buffer, std::string& removed, int lastCharSize, std::function<int (int, std::string&)> encodeChar)
 	{
 		buffer.resize(buffer.size() - lastCharSize);
 		context.setCurrentPos(context.currentPos() - 1);
 		int c = context.currentChar();
-		lastCharSize = EncodeChar(c, removed);
+		lastCharSize = encodeChar(c, removed);
 		context.resetSymbolInfo(); //Deal with possible reduction in symbol size
 		return lastCharSize;
 	}
@@ -513,11 +513,11 @@ namespace C40Encoder {
 				std::string removed;
 				if ((buffer.length() % 3) == 2) {
 					if (available < 2 || available > 2) {
-						lastCharSize = BacktrackOneCharacter(context, buffer, removed, lastCharSize);
+						lastCharSize = BacktrackOneCharacter(context, buffer, removed, lastCharSize, encodeChar);
 					}
 				}
 				while ((buffer.length() % 3) == 1 && ((lastCharSize <= 3 && available != 1) || lastCharSize > 3)) {
-					lastCharSize = BacktrackOneCharacter(context, buffer, removed, lastCharSize);
+					lastCharSize = BacktrackOneCharacter(context, buffer, removed, lastCharSize, encodeChar);
 				}
 				break;
 			}
