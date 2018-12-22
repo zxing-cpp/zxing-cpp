c78af5787841e0fda0af805db98ecbb13a301452
huy <huycuong.nguyen@gmail.com>
2018-12-22 09:36:29 -0500

    Update BuildWinCom.bat to build with VS 2017

--- wrappers/winrt/BuildWinCom.bat
+++ wrappers/winrt/BuildWinCom.bat
@@ -13,7 +13,7 @@ popd
 set BUILD_LOC=%~dp0..\..\build_uwp_x86
 md %BUILD_LOC%
 pushd %BUILD_LOC%
-%CMAKE_EXE% -G "Visual Studio 14 2015" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 -DEXTENSION_SDK_OUTPUT="%DESTINATION%" ..\wrappers\winrt
+%CMAKE_EXE% -G "Visual Studio 15 2017" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 -DEXTENSION_SDK_OUTPUT="%DESTINATION%" ..\wrappers\winrt
 %CMAKE_EXE% --build . --config Release
 %CMAKE_EXE% --build . --config RelWithDebInfo
 popd
@@ -22,7 +22,7 @@ rd /s /q %BUILD_LOC%
 set BUILD_LOC=%~dp0..\..\build_uwp_x64
 md %BUILD_LOC%
 pushd %BUILD_LOC%
-%CMAKE_EXE% -G "Visual Studio 14 2015 Win64" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 -DEXTENSION_SDK_OUTPUT="%DESTINATION%" ..\wrappers\winrt
+%CMAKE_EXE% -G "Visual Studio 15 2017 Win64" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 -DEXTENSION_SDK_OUTPUT="%DESTINATION%" ..\wrappers\winrt
 %CMAKE_EXE% --build . --config Release
 %CMAKE_EXE% --build . --config RelWithDebInfo
 popd
@@ -31,7 +31,7 @@ rd /s /q %BUILD_LOC%
 set BUILD_LOC=%~dp0..\..\build_uwp_arm
 md %BUILD_LOC%
 pushd %BUILD_LOC%
-%CMAKE_EXE% -G "Visual Studio 14 2015 ARM" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 -DEXTENSION_SDK_OUTPUT="%DESTINATION%" ..\wrappers\winrt
+%CMAKE_EXE% -G "Visual Studio 15 2017 ARM" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 -DEXTENSION_SDK_OUTPUT="%DESTINATION%" ..\wrappers\winrt
 %CMAKE_EXE% --build . --config Release
 %CMAKE_EXE% --build . --config RelWithDebInfo
 popd
