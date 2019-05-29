5bfc117d75b6c067139b6fcaf79e36348986f688
Timothy Rae <timothy.rae@innovatech.studio>
2019-05-29 11:39:48 +0900

    Add BarcodeFormat enum

--- wrappers/python/zxing.cpp
+++ wrappers/python/zxing.cpp
@@ -21,15 +21,10 @@ namespace py = pybind11;
 // Note: Image is a Numpy array in BGR format (the default opencv format)
 using Image = py::array_t<uint8_t, py::array::c_style>;
 
-std::string decode(const Image& image, bool fastMode, bool tryRotate, std::string singleFormat) {
-
+std::string decode(const Image& image, bool fastMode, bool tryRotate, ZXing::BarcodeFormat format) {
 	ZXing::DecodeHints hints;
 	hints.setShouldTryHarder(!fastMode);
 	hints.setShouldTryRotate(tryRotate);
-	const auto format = ZXing::BarcodeFormatFromString(singleFormat);
-	if(format == ZXing::BarcodeFormat::FORMAT_COUNT && !singleFormat.empty()) {
-		throw std::invalid_argument("Invalid format code: " + singleFormat);
-	}
 	if (format != ZXing::BarcodeFormat::FORMAT_COUNT) {
 		hints.setPossibleFormats({ format });
 	}
@@ -50,10 +45,37 @@ std::string decode(const Image& image, bool fastMode, bool tryRotate, std::strin
 
 PYBIND11_MODULE(zxingcpp, m) {
 	m.doc() = "python bindings for zxingcpp";
+	py::enum_<ZXing::BarcodeFormat>(m, "BarcodeFormat")
+		.value("AZTEC", ZXing::BarcodeFormat::AZTEC)
+		.value("CODABAR", ZXing::BarcodeFormat::CODABAR)
+		.value("CODE_39", ZXing::BarcodeFormat::CODE_39)
+		.value("CODE_93", ZXing::BarcodeFormat::CODE_93)
+		.value("CODE_128", ZXing::BarcodeFormat::CODE_128)
+		.value("DATA_MATRIX", ZXing::BarcodeFormat::DATA_MATRIX)
+		.value("EAN_8", ZXing::BarcodeFormat::EAN_8)
+		.value("EAN_13", ZXing::BarcodeFormat::EAN_13)
+		.value("ITF", ZXing::BarcodeFormat::ITF)
+		.value("MAXICODE", ZXing::BarcodeFormat::MAXICODE)
+		.value("PDF_417", ZXing::BarcodeFormat::PDF_417)
+		.value("QR_CODE", ZXing::BarcodeFormat::QR_CODE)
+		.value("RSS_14", ZXing::BarcodeFormat::RSS_14)
+		.value("RSS_EXPANDED", ZXing::BarcodeFormat::RSS_EXPANDED)
+		.value("UPC_A", ZXing::BarcodeFormat::UPC_A)
+		.value("UPC_E", ZXing::BarcodeFormat::UPC_E)
+		.value("UPC_EAN_EXTENSION", ZXing::BarcodeFormat::UPC_EAN_EXTENSION)
+		.value("FORMAT_COUNT", ZXing::BarcodeFormat::FORMAT_COUNT)
+		.export_values();
 	m.def("decode", &decode, "Decode a barcode from a numpy BGR image array",
 		py::arg("image"),
 		py::arg("fastMode")=false,
 		py::arg("tryRoate")=true,
-		py::arg("format")=""
+		py::arg("format")=ZXing::BarcodeFormat::FORMAT_COUNT
 	);
 }
+
+/*
+	py::class_<Result>(m, "Result")
+		.def(py::init<const std::string &>())
+		.def("setName", &Pet::setName)
+		.def("getName", &Pet::getName);
+*/
