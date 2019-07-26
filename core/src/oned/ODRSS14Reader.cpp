zxing-cpp/zxing-cpp

commit 17284ea7775d9a4da034a3f817320fbb10813de0

Remove 'external' state of ODRowReaders
The DecoderState instances that had to be passed through all decodeRow functions and were the only reason to have dynamic type casting in the library were removed and replaced by standard internal state where required (only in RSS14 and RSSExtended). This required to introduce a reset() function to reset the internal state. This needs to be called before starting to scan a new image.

Makes the code cleaner and is also more in line with upstream.

core/src/oned/ODRSS14Reader.cpp
@@ -17,7 +17,6 @@
 
 #include "oned/ODRSS14Reader.h"
 #include "oned/rss/ODRSSReaderHelper.h"
-#include "oned/rss/ODRSSPair.h"
 #include "BitArray.h"
 #include "Result.h"
 #include "DecodeHints.h"
@@ -53,20 +52,6 @@ static const std::array<FinderCounters, 9> FINDER_PATTERNS = {
 	1,5,7,1,
 	1,3,9,1,
 };
-
-struct RSS14DecodingState : public RowReader::DecodingState
-{
-	std::list<RSS::Pair> possibleLeftPairs;
-	std::list<RSS::Pair> possibleRightPairs;
-};
-
-//private final List<Pair> possibleLeftPairs;
-//private final List<Pair> possibleRightPairs;
-//
-//public RSS14Reader() {
-//	possibleLeftPairs = new ArrayList<>();
-//	possibleRightPairs = new ArrayList<>();
-//}
 
 static BitArray::Range
 FindFinderPattern(const BitArray& row, bool rightFinderPattern, FinderCounters& counters)
@@ -357,7 +342,7 @@ DecodePair(const BitArray& row, bool right, int rowNumber)
 }
 
 static void
-AddOrTally(std::list<RSS::Pair>& possiblePairs, const RSS::Pair& pair)
+AddOrTally(std::vector<RSS::Pair>& possiblePairs, const RSS::Pair& pair)
 {
 	if (!pair.isValid()) {
 		return;
@@ -416,39 +401,21 @@ ConstructResult(const RSS::Pair& leftPair, const RSS::Pair& rightPair)
 }
 
 Result
-RSS14Reader::decodeRow(int rowNumber, const BitArray& row_, std::unique_ptr<DecodingState>& state) const
+RSS14Reader::decodeRow(int rowNumber, const BitArray& row_) const
 {
-	RSS14DecodingState* prevState = nullptr;
-	if (state == nullptr) {
-		state.reset(prevState = new RSS14DecodingState);
-	}
-	else {
-#if !defined(ZX_HAVE_CONFIG)
-		#error "You need to include ZXConfig.h"
-#elif !defined(ZX_NO_RTTI)
-		prevState = dynamic_cast<RSS14DecodingState*>(state.get());
-#else
-		prevState = static_cast<RSS14DecodingState*>(state.get());
-#endif
-	}
-
-	if (prevState == nullptr) {
-		throw std::runtime_error("Invalid state");
-	}
-
 	BitArray row = row_.copy();
-	AddOrTally(prevState->possibleLeftPairs, DecodePair(row, false, rowNumber));
+	AddOrTally(possibleLeftPairs, DecodePair(row, false, rowNumber));
 	row.reverse();
-	AddOrTally(prevState->possibleRightPairs, DecodePair(row, true, rowNumber));
+	AddOrTally(possibleRightPairs, DecodePair(row, true, rowNumber));
 //	row.reverse();
 
 	// To be able to detect "stacked" RSS codes (split over multiple lines)
 	// we need to store the parts we found and try all possible left/right
 	// combinations. To prevent lots of false positives, we require each
 	// pair to have been seen in at least two lines.
-	for (const auto& left : prevState->possibleLeftPairs) {
+	for (const auto& left : possibleLeftPairs) {
 		if (left.count() > 1) {
-			for (const auto& right : prevState->possibleRightPairs) {
+			for (const auto& right : possibleRightPairs) {
 				if (right.count() > 1) {
 					if (CheckChecksum(left, right)) {
 						return ConstructResult(left, right);

core/src/oned/ODRSS14Reader.h
@@ -17,6 +17,9 @@
 */
 
 #include "oned/ODRowReader.h"
+#include "oned/rss/ODRSSPair.h"
+
+#include <vector>
 
 namespace ZXing {
 namespace OneD {
@@ -26,8 +29,16 @@ namespace OneD {
 */
 class RSS14Reader : public RowReader
 {
+	mutable std::vector<RSS::Pair> possibleLeftPairs;
+	mutable std::vector<RSS::Pair> possibleRightPairs;
+
 public:
-	Result decodeRow(int rowNumber, const BitArray& row, std::unique_ptr<DecodingState>& state) const override;
+	Result decodeRow(int rowNumber, const BitArray& row) const override;
+
+	void reset() override {
+		possibleLeftPairs.clear();
+		possibleRightPairs.clear();
+	}
 };
 
 } // OneD

core/src/oned/ODRSSExpandedReader.cpp
@@ -25,7 +25,6 @@
 #include "ZXConfig.h"
 
 #include <cmath>
-#include <list>
 #include <array>
 #include <vector>
 #include <numeric>
@@ -92,12 +91,6 @@ static const std::array<std::vector<int>, 10> FINDER_PATTERN_SEQUENCES = { {
 	{ FINDER_PAT_A, FINDER_PAT_A, FINDER_PAT_B, FINDER_PAT_B, FINDER_PAT_C, FINDER_PAT_C, FINDER_PAT_D, FINDER_PAT_E, FINDER_PAT_F, FINDER_PAT_F },
 	{ FINDER_PAT_A, FINDER_PAT_A, FINDER_PAT_B, FINDER_PAT_B, FINDER_PAT_C, FINDER_PAT_D, FINDER_PAT_D, FINDER_PAT_E, FINDER_PAT_E, FINDER_PAT_F, FINDER_PAT_F },
 } };
-
-
-struct RSSExpandedDecodingState : public RowReader::DecodingState
-{
-	std::list<RSS::ExpandedRow> rows;
-};
 
 
 using namespace RSS;
@@ -710,31 +703,13 @@ ConstructResult(const std::list<ExpandedPair>& pairs)
 }
 
 Result
-RSSExpandedReader::decodeRow(int rowNumber, const BitArray& row, std::unique_ptr<DecodingState>& state) const
+RSSExpandedReader::decodeRow(int rowNumber, const BitArray& row) const
 {
-	RSSExpandedDecodingState* prevState = nullptr;
-	if (state == nullptr) {
-		state.reset(prevState = new RSSExpandedDecodingState);
-	}
-	else {
-#if !defined(ZX_HAVE_CONFIG)
-		#error "You need to include ZXConfig.h"
-#elif !defined(ZX_NO_RTTI)
-		prevState = dynamic_cast<RSSExpandedDecodingState*>(state.get());
-#else
-		prevState = static_cast<RSSExpandedDecodingState*>(state.get());
-#endif
-	}
-
-	if (prevState == nullptr) {
-		throw std::runtime_error("Invalid state");
-	}
-
 	// Rows can start with even pattern in case in prev rows there where odd number of patters.
 	// So lets try twice
-	Result r = ConstructResult(DecodeRow2Pairs(rowNumber, row, false, prevState->rows));
+	Result r = ConstructResult(DecodeRow2Pairs(rowNumber, row, false, rows));
 	if (!r.isValid()) {
-		r = ConstructResult(DecodeRow2Pairs(rowNumber, row, true, prevState->rows));
+		r = ConstructResult(DecodeRow2Pairs(rowNumber, row, true, rows));
 	}
 	return r;
 }

core/src/oned/ODRSSExpandedReader.h
@@ -17,6 +17,9 @@
 */
 
 #include "oned/ODRowReader.h"
+#include "oned/rss/ODRSSExpandedRow.h"
+
+#include <list>
 
 namespace ZXing {
 namespace OneD {
@@ -27,8 +30,14 @@ namespace OneD {
 */
 class RSSExpandedReader : public RowReader
 {
+	mutable std::list<RSS::ExpandedRow> rows;
+
 public:
-	Result decodeRow(int rowNumber, const BitArray& row, std::unique_ptr<DecodingState>& state) const override;
+	Result decodeRow(int rowNumber, const BitArray& row) const override;
+
+	void reset() override {
+		rows.clear();
+	}
 };
 
 } // OneD

core/src/oned/ODRowReader.h
@@ -39,14 +39,14 @@ class RowReader
 {
 public:
 
-	struct DecodingState
-	{
-		virtual ~DecodingState() {}
-	};
-
-	Result decodeSingleRow(int rowNumber, const BitArray& row) const;
-
 	virtual ~RowReader() {}
+
+	/**
+	 * @brief resets the internal state which some readers have to be able to decode multi-line variantes (e.g. RSS).
+	 *
+	 * Call before starting to process a new image.
+	 */
+	virtual void reset() {}
 
 	/**
 	* <p>Attempts to decode a one-dimensional barcode format given a single row of
@@ -60,7 +60,7 @@ public:
 	* @throws ChecksumException if a potential barcode is found but does not pass its checksum
 	* @throws FormatException if a potential barcode is found but format is invalid
 	*/
-	virtual Result decodeRow(int rowNumber, const BitArray& row, std::unique_ptr<DecodingState>& state) const = 0;
+	virtual Result decodeRow(int rowNumber, const BitArray& row) const = 0;
 
 	/**
 	* Scans the given bit range for a pattern identified by evaluating the function object match for each

core/src/ZXConfig.h
@@ -21,10 +21,6 @@
 
 #define ZX_HAVE_CONFIG
 
-#if !__has_attribute(cxx_rtti) && !defined(__RTTI) && !defined(_CPPRTTI) && !defined(__GXX_RTTI) && !defined(__INTEL_RTTI__)
-	#define ZX_NO_RTTI
-#endif
-
 // Thread local or static memory may be used to reduce the number of (re-)allocations of temporary variables
 // in e.g. the ReedSolomonDecoder. It is disabled by default. It can be enabled by modifying the following define.
 // Note: The Apple clang compiler until XCode 8 does not support c++11's thread_local.

core/src/oned/ODCodabarReader.cpp
@@ -221,7 +221,7 @@ CodabarReader::CodabarReader(const DecodeHints& hints)
 }
 
 Result
-CodabarReader::decodeRow(int rowNumber, const BitArray& row, std::unique_ptr<DecodingState>&) const
+CodabarReader::decodeRow(int rowNumber, const BitArray& row) const
 {
 	std::vector<int> counters;
 	counters.reserve(80);

core/src/oned/ODCodabarReader.h
@@ -34,7 +34,7 @@ class CodabarReader : public RowReader
 {
 public:
 	explicit CodabarReader(const DecodeHints& hints);
-	Result decodeRow(int rowNumber, const BitArray& row, std::unique_ptr<DecodingState>& state) const override;
+	Result decodeRow(int rowNumber, const BitArray& row) const override;
 
 private:
 	bool _shouldReturnStartEnd;

commit 2da4f0e4bf9c8906ea750b482d0ef05bfb671381

fix thread-safety regression in RSS* decoders
Introduce little ThreadLocal template helper to implement some kind of 'thread local storage' for the RSS-type readers that support 'stacked' 1D codes (i.e. 2D codes actually) and need to store some state between decoding different lines.

This is a fix for the thread-safety regression introduced in 17284ea7775d9a4da034a3f817320fbb10813de0

core/src/oned/ODRSS14Reader.cpp
@@ -403,19 +403,20 @@ ConstructResult(const RSS::Pair& leftPair, const RSS::Pair& rightPair)
 Result
 RSS14Reader::decodeRow(int rowNumber, const BitArray& row_) const
 {
+	auto& possiblePairs = state();
 	BitArray row = row_.copy();
-	AddOrTally(possibleLeftPairs, DecodePair(row, false, rowNumber));
+	AddOrTally(possiblePairs.left, DecodePair(row, false, rowNumber));
 	row.reverse();
-	AddOrTally(possibleRightPairs, DecodePair(row, true, rowNumber));
+	AddOrTally(possiblePairs.right, DecodePair(row, true, rowNumber));
 //	row.reverse();
 
 	// To be able to detect "stacked" RSS codes (split over multiple lines)
 	// we need to store the parts we found and try all possible left/right
 	// combinations. To prevent lots of false positives, we require each
 	// pair to have been seen in at least two lines.
-	for (const auto& left : possibleLeftPairs) {
+	for (const auto& left : possiblePairs.left) {
 		if (left.count() > 1) {
-			for (const auto& right : possibleRightPairs) {
+			for (const auto& right : possiblePairs.right) {
 				if (right.count() > 1) {
 					if (CheckChecksum(left, right)) {
 						return ConstructResult(left, right);

core/src/oned/ODRSS14Reader.h
@@ -29,15 +29,20 @@ namespace OneD {
 */
 class RSS14Reader : public RowReader
 {
-	mutable std::vector<RSS::Pair> possibleLeftPairs;
-	mutable std::vector<RSS::Pair> possibleRightPairs;
+	struct State
+	{
+		std::vector<RSS::Pair> left, right;
+	};
+
+	mutable ThreadLocal<State> state;
 
 public:
 	Result decodeRow(int rowNumber, const BitArray& row) const override;
 
 	void reset() override {
-		possibleLeftPairs.clear();
-		possibleRightPairs.clear();
+		auto& possiblePairs = state();
+		possiblePairs.left.clear();
+		possiblePairs.right.clear();
 	}
 };
 

core/src/oned/ODRSSExpandedReader.cpp
@@ -707,6 +707,7 @@ RSSExpandedReader::decodeRow(int rowNumber, const BitArray& row) const
 {
 	// Rows can start with even pattern in case in prev rows there where odd number of patters.
 	// So lets try twice
+	auto& rows = this->rows();
 	Result r = ConstructResult(DecodeRow2Pairs(rowNumber, row, false, rows));
 	if (!r.isValid()) {
 		r = ConstructResult(DecodeRow2Pairs(rowNumber, row, true, rows));

core/src/oned/ODRSSExpandedReader.h
@@ -30,13 +30,13 @@ namespace OneD {
 */
 class RSSExpandedReader : public RowReader
 {
-	mutable std::list<RSS::ExpandedRow> rows;
+	mutable ThreadLocal<std::list<RSS::ExpandedRow>> rows;
 
 public:
 	Result decodeRow(int rowNumber, const BitArray& row) const override;
 
 	void reset() override {
-		rows.clear();
+		rows().clear();
 	}
 };
 

core/src/oned/ODRowReader.h
@@ -22,6 +22,10 @@
 #include <cstddef>
 #include <memory>
 
+#include <thread>
+#include <mutex>
+#include <map>
+
 namespace ZXing {
 
 class Result;
@@ -37,12 +41,29 @@ namespace OneD {
 */
 class RowReader
 {
+protected:
+	/**
+	 * @brief Helper template for thread local type storage for per image state data of multi-line "1D" readers (like RSS)
+	 */
+	template<typename T>
+	class ThreadLocal
+	{
+		std::map<std::thread::id, T> store;
+		std::mutex mut;
+	public:
+		T& operator()()
+		{
+			std::lock_guard<std::mutex> lock(mut);
+			return store[std::this_thread::get_id()];
+		}
+	};
+
 public:
 
 	virtual ~RowReader() {}
 
 	/**
-	 * @brief resets the internal state which some readers have to be able to decode multi-line variantes (e.g. RSS).
+	 * @brief resets the internal state which some readers have to be able to decode multi-line variants (e.g. RSS).
 	 *
 	 * Call before starting to process a new image.
 	 */