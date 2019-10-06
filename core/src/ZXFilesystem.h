46a4b60337adaecc90d1b85bfe47b49c6bb1f1ca
Axel Waggershauser <awagger@gmail.com>
2019-10-06 23:31:15 +0200

    add compiler warning if required std::filesystem can not be found

--- core/src/ZXFilesystem.h
+++ core/src/ZXFilesystem.h
@@ -28,5 +28,9 @@
 #  endif
 #endif
 
+#if !defined(__cpp_lib_filesystem) && !defined(__cpp_lib_experimental_filesystem)
+#  error need standard filesystem library from c++-17 or the Filesystem TR
+#endif
+
 // compiling this with clang (e.g. version 6) might require linking against libc++experimental.a or libc++fs.a.
 // E.g.: CMAKE_EXE_LINKER_FLAGS = -L/usr/local/Cellar/llvm/6.0.1/lib -lc++experimental
