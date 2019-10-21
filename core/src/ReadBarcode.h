cce1b74ef218ab9299426eb52bd62a58fb7df650
Stéphane Cerveau <scerveau@gmail.com>
2019-10-21 16:52:31 +0200

    ReadBarcode: update doc to use JavaDoc syntax

--- core/src/ReadBarcode.h
+++ core/src/ReadBarcode.h
@@ -24,15 +24,43 @@
 namespace ZXing {
 
 /**
- * Standalone API to read barcode from a grayscale buffer
+ * Read barcode from a grayscale buffer
  *
+ * <p>Use {@link #ReadBarcode(int width, int height, unsigned char* data, int rowStride,
+				   std::vector<BarcodeFormat> formats = {}, bool tryRotate = true, bool tryHarder = true) to read a bar code from a buffer.
+ *
+ * @param width image width
+ * @param height image height
+ * @param data   image buffer
+ * @param rowstride  row stride
+ * @param formats   A list of format to search for ( faster)
+ * @param tryRotate   try to rotate the buffer to find the barcode (slower)
+ * @param tryHarder   try harder to find the barcode(slower). TODO needs to explain
+ * @return            #Result structure
+ * @since             0.x
  */
 Result ReadBarcode(int width, int height, unsigned char* data, int rowStride,
 				   std::vector<BarcodeFormat> formats = {}, bool tryRotate = true, bool tryHarder = true);
 
 /**
- * Standalone API to read barcode from a RGB buffer
+ * Read barcode from a RGB buffer
  *
+ * <p>Use {@link #ReadBarcode(int width, int height, unsigned char* data, int rowStride, int pixelStride, int rIndex, int gIndex, int bIndex,
+				   std::vector<BarcodeFormat> formats = {}, bool tryRotate = true, bool tryHarder = true) to read a bar code from a buffer.
+ *
+ * @param width image width
+ * @param height image height
+ * @param data   image buffer
+ * @param rowstride  row stride
+ * @param pixelstride  pixel stride (ie 4 for 32 bits)
+ * @param rIndex  red index
+ * @param gIndex  green index
+ * @param bIndex  blue index
+ * @param formats   A list of format to search for ( faster)
+ * @param tryRotate   try to rotate the buffer to find the barcode (slower)
+ * @param tryHarder   try harder to find the barcode(slower). TODO needs to explain
+ * @return            #Result structure
+ * @since             0.x
  */
 Result ReadBarcode(int width, int height, unsigned char* data, int rowStride, int pixelStride, int rIndex, int gIndex, int bIndex,
 				   std::vector<BarcodeFormat> formats = {}, bool tryRotate = true, bool tryHarder = true);
