3578416832ad2eeaba0bf5e67109090673bbb611
Axel Waggershauser <awagger@gmail.com>
2020-06-05 09:49:43 +0200

    python: add demo_reader.py usage example

---
+++ wrappers/python/demo_reader.py
@@ -0,0 +1,10 @@
+import sys, zxing
+from PIL import Image
+
+img = Image.open(sys.argv[1])
+result = zxing.decode(img)
+if result.valid:
+    print("Found barcode with value '{}' (format: {})".format(result.text, str(result.format))) 
+else:
+    print("Could not read barcode")
+
