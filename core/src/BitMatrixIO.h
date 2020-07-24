e2462bf59adfc5b06d212cc580ca635e17fc18ce
Axel Waggershauser <awagger@gmail.com>
2020-07-24 16:08:03 +0200

    BitMatrixIO: set default quite-zone in SaveAsPBM to 0
    This has cost me about 2 hours of debugging a non-existent off-by-one
    error. grrrrr.

--- core/src/BitMatrixIO.h
+++ core/src/BitMatrixIO.h
@@ -24,6 +24,6 @@ namespace ZXing {
     
 	std::string ToString(const BitMatrix& matrix, char one = 'X', char zero = ' ', bool addSpace = true, bool printAsCString = false);
 	BitMatrix ParseBitMatrix(const std::string& str, char one = 'X', bool expectSpace = true);
-	void SaveAsPBM(const BitMatrix& matrix, const std::string filename, int quiteZone = 1);
+	void SaveAsPBM(const BitMatrix& matrix, const std::string filename, int quiteZone = 0);
 
 } // ZXing
