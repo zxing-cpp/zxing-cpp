f1904081383ea4c101aa5b8eba6ba4ac574d45cf
Axel Waggershauser <awagger@gmail.com>
2020-10-05 10:02:48 +0200

    test: don't explicitly request Code39 format for code39-2 tests

--- test/blackbox/BlackboxTestRunner.cpp
+++ test/blackbox/BlackboxTestRunner.cpp
@@ -344,7 +344,7 @@ int runBlackBoxTests(const fs::path& testPathPrefix, const std::set<std::string>
 		runTests("code39-2", "Code39", 2, {
 			{ 2, 2, 0   },
 			{ 2, 2, 180 },
-		}, DecodeHints().setTryCode39ExtendedMode(true).setFormats(BarcodeFormat::Code39));
+		}, DecodeHints().setTryCode39ExtendedMode(true));
 
 		runTests("code39-3", "Code39", 12, {
 			{ 12, 12, 0   },
