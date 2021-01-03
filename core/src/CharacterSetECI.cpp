433341f912edf2788cf6bd966063180bcc0aac69
Huy Cuong Nguyen <huycn@users.noreply.github.com>
2021-01-03 16:23:03 -0500

    Fix failure to parse text encoding with less intrusive changes

--- core/src/CharacterSetECI.cpp
+++ core/src/CharacterSetECI.cpp
@@ -19,7 +19,6 @@
 
 #include "CharacterSet.h"
 
-#include <string>
 #include <cctype>
 #include <map>
 #include <utility>
@@ -60,8 +59,24 @@ static const std::map<int, CharacterSet> ECI_VALUE_TO_CHARSET = {
 	{170, CharacterSet::ASCII},
 };
 
-static const std::map<std::string, CharacterSet> ECI_NAME_TO_CHARSET = {
-	{"CP437",		CharacterSet::Cp437},
+struct CompareNoCase {
+	bool operator ()(const char* a, const char* b) const {
+		while (*a != '\0' && *b != '\0') {
+			auto ca = std::tolower(*a++);
+			auto cb = std::tolower(*b++);
+			if (ca < cb) {
+				return true;
+			}
+			else if (ca > cb) {
+				return false;
+			}
+		}
+		return *a == '\0' && *b != '\0';
+	}
+};
+
+static const std::map<const char *, CharacterSet, CompareNoCase> ECI_NAME_TO_CHARSET = {
+	{"Cp437",		CharacterSet::Cp437},
 	{"ISO8859_1",	CharacterSet::ISO8859_1},
 	{"ISO-8859-1",	CharacterSet::ISO8859_1},
 	{"ISO8859_2",	CharacterSet::ISO8859_2},
@@ -93,23 +108,23 @@ static const std::map<std::string, CharacterSet> ECI_NAME_TO_CHARSET = {
 	{"ISO8859_16",	CharacterSet::ISO8859_16},
 	{"ISO-8859-16",	CharacterSet::ISO8859_16},
 	{"SJIS",		CharacterSet::Shift_JIS},
-	{"SHIFT_JIS",	CharacterSet::Shift_JIS},
-	{"CP1250",		CharacterSet::Cp1250},
-	{"WINDOWS-1250",CharacterSet::Cp1250},
-	{"CP1251",		CharacterSet::Cp1251},
-	{"WINDOWS-1251",CharacterSet::Cp1251},
-	{"CP1252",		CharacterSet::Cp1252},
-	{"WINDOWS-1252",CharacterSet::Cp1252},
-	{"CP1256",		CharacterSet::Cp1256},
-	{"WINDOWS-1256",CharacterSet::Cp1256},
-	{"UNICODEBIGUNMARKED", CharacterSet::UnicodeBig},
+	{"Shift_JIS",	CharacterSet::Shift_JIS},
+	{"Cp1250",		CharacterSet::Cp1250},
+	{"windows-1250",CharacterSet::Cp1250},
+	{"Cp1251",		CharacterSet::Cp1251},
+	{"windows-1251",CharacterSet::Cp1251},
+	{"Cp1252",		CharacterSet::Cp1252},
+	{"windows-1252",CharacterSet::Cp1252},
+	{"Cp1256",		CharacterSet::Cp1256},
+	{"windows-1256",CharacterSet::Cp1256},
+	{"UnicodeBigUnmarked", CharacterSet::UnicodeBig},
 	{"UTF-16BE",	CharacterSet::UnicodeBig},
-	{"UNICODEBIG",	CharacterSet::UnicodeBig},
+	{"UnicodeBig",	CharacterSet::UnicodeBig},
 	{"UTF8",		CharacterSet::UTF8},
 	{"UTF-8",		CharacterSet::UTF8},
 	{"ASCII",		CharacterSet::ASCII},
 	{"US-ASCII",	CharacterSet::ASCII},
-	{"BIG5",		CharacterSet::Big5},
+	{"Big5",		CharacterSet::Big5},
 	{"GB2312",		CharacterSet::GB2312},
 	{"GB18030",		CharacterSet::GB18030},
 	{"EUC_CN",		CharacterSet::GB18030},
@@ -138,14 +153,9 @@ int ValueForCharset(CharacterSet charset)
 	return 0;
 }
 
-static std::string toUpper(std::string s) {
-	std::transform(s.begin(), s.end(), s.begin(), [](char c) { return static_cast<char>(std::toupper(c)); });
-	return s;
-}
-
 CharacterSet CharsetFromName(const char* name)
 {
-	auto it = ECI_NAME_TO_CHARSET.find(toUpper(name));
+	auto it = ECI_NAME_TO_CHARSET.find(name);
 	if (it != ECI_NAME_TO_CHARSET.end()) {
 		return it->second;
 	}
