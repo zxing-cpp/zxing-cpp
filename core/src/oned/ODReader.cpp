98f9990896902a45625a326fa58b5dbbe6ef4991
Axel Waggershauser <awagger@gmail.com>
2021-07-22 13:03:58 +0200

    Multi-Barcode: increase scanning density in tryHarder use case

--- core/src/oned/ODReader.cpp
+++ core/src/oned/ODReader.cpp
@@ -88,7 +88,8 @@ static Results DoDecode(const std::vector<std::unique_ptr<RowReader>>& readers, 
 		std::swap(width, height);
 
 	int middle = height / 2;
-	int rowStep = std::max(1, height / (tryHarder ? 256 : 32));
+	// TODO: find a better heuristic/parameterization if maxSymbols != 1
+	int rowStep = std::max(1, height / (tryHarder ? (maxSymbols == 1 ? 256 : 512) : 32));
 	int maxLines = tryHarder ?
 		height :	// Look at the whole image, not just the center
 		15;			// 15 rows spaced 1/32 apart is roughly the middle half of the image
