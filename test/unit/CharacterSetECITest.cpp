e4604f690a66e04a12a39e831f68972eaf969e6b
axxel <awagger@gmail.com>
2022-06-03 17:49:25 +0200

    CharacterSet: introduce name normalization like in BarcodeFormatFromString

--- test/unit/CharacterSetECITest.cpp
+++ test/unit/CharacterSetECITest.cpp
@@ -3,6 +3,7 @@
 */
 // SPDX-License-Identifier: Apache-2.0
 
+#include "CharacterSet.h"
 #include "ECI.h"
 
 #include "gtest/gtest.h"
@@ -19,4 +20,13 @@ TEST(CharacterSetECITest, Charset2ECI)
 	EXPECT_EQ(ToInt(ToECI(CharacterSet::EUC_KR)), 30);
 	EXPECT_EQ(ToInt(ToECI(CharacterSet::BINARY)), 899);
 	EXPECT_EQ(ToInt(ToECI(CharacterSet::Unknown)), -1);
+}
+
+TEST(CharacterSetECITest, CharacterSetFromString)
+{
+	EXPECT_EQ(CharacterSet::ISO8859_1, CharacterSetFromString("ISO-8859-1"));
+	EXPECT_EQ(CharacterSet::ISO8859_1, CharacterSetFromString("ISO8859_1"));
+	EXPECT_EQ(CharacterSet::ISO8859_1, CharacterSetFromString("ISO 8859-1"));
+	EXPECT_EQ(CharacterSet::ISO8859_1, CharacterSetFromString("iso88591"));
+	EXPECT_EQ(CharacterSet::Unknown, CharacterSetFromString("invalid-name"));
 }
