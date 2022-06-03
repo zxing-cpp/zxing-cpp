0559973638a2cc9e871b570fd334884b7d3dae39
axxel <awagger@gmail.com>
2022-06-03 17:49:25 +0200

    CharacterSet: introduce name normalization like in BarcodeFormatFromString

--- core/src/CharacterSet.cpp
+++ core/src/CharacterSet.cpp
@@ -6,59 +6,34 @@
 
 #include "CharacterSet.h"
 
-#include <cctype>
-#include <map>
-#include <utility>
+#include "ZXContainerAlgorithms.h"
+
+#include <algorithm>
 
 namespace ZXing {
 
-struct CompareNoCase {
-	bool operator ()(const char* a, const char* b) const {
-		while (*a != '\0' && *b != '\0') {
-			auto ca = std::tolower(*a++);
-			auto cb = std::tolower(*b++);
-			if (ca < cb) {
-				return true;
-			}
-			else if (ca > cb) {
-				return false;
-			}
-		}
-		return *a == '\0' && *b != '\0';
-	}
+struct CharacterSetName
+{
+	const char* name;
+	CharacterSet cs;
 };
 
-static const std::map<const char *, CharacterSet, CompareNoCase> ECI_NAME_TO_CHARSET = {
+static CharacterSetName NAME_TO_CHARSET[] = {
 	{"Cp437",		CharacterSet::Cp437},
-	{"ISO8859_1",	CharacterSet::ISO8859_1},
 	{"ISO-8859-1",	CharacterSet::ISO8859_1},
-	{"ISO8859_2",	CharacterSet::ISO8859_2},
 	{"ISO-8859-2",	CharacterSet::ISO8859_2},
-	{"ISO8859_3",	CharacterSet::ISO8859_3},
 	{"ISO-8859-3",	CharacterSet::ISO8859_3},
-	{"ISO8859_4",	CharacterSet::ISO8859_4},
 	{"ISO-8859-4",	CharacterSet::ISO8859_4},
-	{"ISO8859_5",	CharacterSet::ISO8859_5},
 	{"ISO-8859-5",	CharacterSet::ISO8859_5},
-	{"ISO8859_6",	CharacterSet::ISO8859_6},
 	{"ISO-8859-6",	CharacterSet::ISO8859_6},
-	{"ISO8859_7",	CharacterSet::ISO8859_7},
 	{"ISO-8859-7",	CharacterSet::ISO8859_7},
-	{"ISO8859_8",	CharacterSet::ISO8859_8},
 	{"ISO-8859-8",	CharacterSet::ISO8859_8},
-	{"ISO8859_9",	CharacterSet::ISO8859_9},
 	{"ISO-8859-9",	CharacterSet::ISO8859_9},
-	{"ISO8859_10",	CharacterSet::ISO8859_10},
 	{"ISO-8859-10",	CharacterSet::ISO8859_10},
-	{"ISO8859_11",	CharacterSet::ISO8859_11},
 	{"ISO-8859-11",	CharacterSet::ISO8859_11},
-	{"ISO8859_13",	CharacterSet::ISO8859_13},
 	{"ISO-8859-13",	CharacterSet::ISO8859_13},
-	{"ISO8859_14",	CharacterSet::ISO8859_14},
 	{"ISO-8859-14",	CharacterSet::ISO8859_14},
-	{"ISO8859_15",	CharacterSet::ISO8859_15},
 	{"ISO-8859-15",	CharacterSet::ISO8859_15},
-	{"ISO8859_16",	CharacterSet::ISO8859_16},
 	{"ISO-8859-16",	CharacterSet::ISO8859_16},
 	{"SJIS",		CharacterSet::Shift_JIS},
 	{"Shift_JIS",	CharacterSet::Shift_JIS},
@@ -73,28 +48,29 @@ static const std::map<const char *, CharacterSet, CompareNoCase> ECI_NAME_TO_CHA
 	{"UnicodeBigUnmarked", CharacterSet::UnicodeBig},
 	{"UTF-16BE",	CharacterSet::UnicodeBig},
 	{"UnicodeBig",	CharacterSet::UnicodeBig},
-	{"UTF8",		CharacterSet::UTF8},
 	{"UTF-8",		CharacterSet::UTF8},
 	{"ASCII",		CharacterSet::ASCII},
 	{"US-ASCII",	CharacterSet::ASCII},
 	{"Big5",		CharacterSet::Big5},
 	{"GB2312",		CharacterSet::GB2312},
 	{"GB18030",		CharacterSet::GB18030},
-	{"EUC_CN",		CharacterSet::GB18030},
 	{"EUC-CN",		CharacterSet::GB18030},
 	{"GBK",			CharacterSet::GB18030},
-	{"EUC_KR",		CharacterSet::EUC_KR},
 	{"EUC-KR",		CharacterSet::EUC_KR},
 	{"BINARY",		CharacterSet::BINARY},
 };
 
+static std::string NormalizeName(std::string str)
+{
+	std::transform(str.begin(), str.end(), str.begin(), [](char c) { return (char)std::tolower(c); });
+	str.erase(std::remove_if(str.begin(), str.end(), [](char c) { return Contains("_-[] ", c); }), str.end());
+	return str;
+}
+
 CharacterSet CharacterSetFromString(const std::string& name)
 {
-	auto it = ECI_NAME_TO_CHARSET.find(name.c_str());
-	if (it != ECI_NAME_TO_CHARSET.end())
-		return it->second;
-
-	return CharacterSet::Unknown;
+	auto i = FindIf(NAME_TO_CHARSET, [str = NormalizeName(name)](auto& v) { return NormalizeName(v.name) == str; });
+	return i == std::end(NAME_TO_CHARSET) ? CharacterSet::Unknown : i->cs;
 }
 
 } // namespace ZXing
