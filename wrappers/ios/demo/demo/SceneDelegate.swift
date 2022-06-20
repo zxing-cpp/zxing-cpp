b43cc2bc12946a0a9b09cd3f9b87963d3c272124
Christian Braun <braun.christian.bamberg@gmail.com>
2022-06-20 15:33:41 +0200

    Remove whitelines

--- wrappers/ios/demo/demo/SceneDelegate.swift
+++ wrappers/ios/demo/demo/SceneDelegate.swift
@@ -10,7 +10,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
@@ -45,7 +44,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
-
