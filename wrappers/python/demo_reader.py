9bb417d45e0782f0468f2f83f1c14038a1f0279d
axxel <awagger@gmail.com>
2022-06-28 23:58:56 +0200

    python: add content_type output to demo_reader.py

--- wrappers/python/demo_reader.py
+++ wrappers/python/demo_reader.py
@@ -4,7 +4,7 @@ from PIL import Image
 img = Image.open(sys.argv[1])
 results = zxingcpp.read_barcodes(img)
 for result in results:
-	print("Found barcode:\n Text:    '{}'\n Format:   {}\n Position: {}"
-		.format(result.text, result.format, result.position))
+	print("Found barcode:\n Text:    '{}'\n Format:   {}\n Content:  {}\n Position: {}"
+		.format(result.text, result.format, result.content_type, result.position))
 if len(results) == 0:
 	print("Could not find any barcode.")
