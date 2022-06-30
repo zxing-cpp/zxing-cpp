0da93e0983eea30dcfefdcfbf5832acab6de5d94
axxel <awagger@gmail.com>
2022-07-01 00:43:14 +0200

    Error: implement typesafe comparison between Error and Error::Type
    Prevent a comparison of `Error` and `Error::Type` (via `operator bool()`)
    by changing `enum Type` to `enum class Type`.

--- core/src/Error.h
+++ core/src/Error.h
@@ -12,19 +12,28 @@ namespace ZXing {
 class Error
 {
 public:
-	enum Type { None, Format, Checksum, Unsupported };
+	enum class Type { None, Format, Checksum, Unsupported };
 	Type type() const noexcept { return _type; }
 	const std::string& msg() const noexcept { return _msg; }
-	operator bool() const noexcept { return _type != None; }
+	operator bool() const noexcept { return _type != Type::None; }
 
 	Error() = default;
 
+	static constexpr auto Format = Type::Format;
+	static constexpr auto Checksum = Type::Checksum;
+	static constexpr auto Unsupported = Type::Unsupported;
+
 protected:
-	Type _type = None;
+	Type _type = Type::None;
 	std::string _msg;
 
 	Error(Type type, std::string msg) : _type(type), _msg(std::move(msg)) {}
 };
+
+inline bool operator==(const Error& e, Error::Type t) noexcept { return e.type() == t; }
+inline bool operator!=(const Error& e, Error::Type t) noexcept { return !(e == t); }
+inline bool operator==(Error::Type t, const Error& e) noexcept { return e.type() == t; }
+inline bool operator!=(Error::Type t, const Error& e) noexcept { return !(t == e); }
 
 class FormatError : public Error
 {
