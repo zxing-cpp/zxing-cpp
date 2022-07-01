24b737365358bdb0e404e65cb70d0071362159f7
axxel <awagger@gmail.com>
2022-07-01 16:17:16 +0200

    GS1: introduce general purpose `HRIFromGS1` function
    This a cleaned up and generalized (handle `<GS>` delimiters) version of
    what was formerly implemented as part of the DataBar decoder.

---
+++ core/src/GS1.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2022 Axel Waggershauser
+ */
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include <string>
+
+namespace ZXing {
+
+std::string HRIFromGS1(const std::string& gs1);
+
+} // namespace ZXing
