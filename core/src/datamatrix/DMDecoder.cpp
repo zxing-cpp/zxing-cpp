576bfba144ba1d6d4e22f25d2c6f856e67f2a320
Alexander Kozlov <bobokvsky@epoch8.co>
2022-07-04 23:04:32 +0000

    add FNC1 hack

--- core/src/datamatrix/DMDecoder.cpp
+++ core/src/datamatrix/DMDecoder.cpp
@@ -301,12 +301,18 @@ DecoderResult Decode(ByteArray&& bytes, const bool isDMRE)
 				// Only recognizing an FNC1 as first/second by codeword position (aka symbol character position), not
 				// by decoded character position, i.e. not recognizing a C40/Text encoded FNC1 (which requires a latch
 				// and a shift)
-				if (bits.byteOffset() == firstFNC1Position)
-					result.symbology.modifier = '2'; // GS1
-				else if (bits.byteOffset() == firstFNC1Position + 1)
-					result.symbology.modifier = '3'; // AIM, note no AIM Application Indicator format defined, ISO 16022:2006 11.2
-				else
-					result.push_back((char)29); // translate as ASCII 29 <GS>
+				// if (bits.byteOffset() == firstFNC1Position)
+				// 	result.symbology.modifier = '2'; // GS1
+				// else if (bits.byteOffset() == firstFNC1Position + 1)
+				// 	result.symbology.modifier = '3'; // AIM, note no AIM Application Indicator format defined, ISO 16022:2006 11.2
+				// else
+					// result.push_back((char)29); // translate as ASCII 29 <GS>
+                result.push_back((char)'{');
+                result.push_back((char)'F');
+                result.push_back((char)'N');
+                result.push_back((char)'C');
+                result.push_back((char)'1');
+                result.push_back((char)'}');
 				break;
 			case 233: // Structured Append
 				if (!firstCodeword) // Must be first ISO 16022:2006 5.6.1
