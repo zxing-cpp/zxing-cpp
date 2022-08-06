5c783c74238f953ef0eaa055bc6196b2d361050a
axxel <awagger@gmail.com>
2022-08-06 12:05:58 +0200

    ContentTest: replace utf16 with utf8 usage

--- test/unit/ContentTest.cpp
+++ test/unit/ContentTest.cpp
@@ -25,14 +25,14 @@ TEST(ContentTest, Base)
 		Content c;
 		c.switchEncoding(CharacterSet::ISO8859_1);
 		c.append(ByteArray{'A', 0xE9, 'Z'});
-		EXPECT_EQ(c.utf16(), L"A\u00E9Z");
+		EXPECT_EQ(c.utf8(), u8"A\u00E9Z");
 	}
 
 	{ // set CharacterSet::ISO8859_5
 		Content c;
 		c.switchEncoding(CharacterSet::ISO8859_5);
 		c.append(ByteArray{'A', 0xE9, 'Z'});
-		EXPECT_EQ(c.utf16(), L"A\u0449Z");
+		EXPECT_EQ(c.utf8(), u8"A\u0449Z");
 	}
 
 	{ // switch to CharacterSet::ISO8859_5
@@ -42,7 +42,7 @@ TEST(ContentTest, Base)
 		c.switchEncoding(CharacterSet::ISO8859_5);
 		EXPECT_FALSE(c.hasECI);
 		c.append(ByteArray{'A', 0xE9, 'Z'});
-		EXPECT_EQ(c.utf16(), L"A\u00E9ZA\u0449Z");
+		EXPECT_EQ(c.utf8(), u8"A\u00E9ZA\u0449Z");
 	}
 }
 
@@ -52,7 +52,7 @@ TEST(ContentTest, GuessEncoding)
 		Content c;
 		c.append(ByteArray{'A', 0xE9, 'Z'});
 		EXPECT_EQ(c.guessEncoding(), CharacterSet::ISO8859_1);
-		EXPECT_EQ(c.utf16(), L"A\u00E9Z");
+		EXPECT_EQ(c.utf8(), u8"A\u00E9Z");
 		EXPECT_EQ(c.bytesECI(), c.bytes);
 	}
 
@@ -60,7 +60,7 @@ TEST(ContentTest, GuessEncoding)
 		Content c;
 		c.append(ByteArray{'A', 0x83, 0x65, 'Z'});
 		EXPECT_EQ(c.guessEncoding(), CharacterSet::Shift_JIS);
-		EXPECT_EQ(c.utf16(), L"A\u30C6Z");
+		EXPECT_EQ(c.utf8(), u8"A\u30C6Z");
 	}
 }
 
@@ -72,7 +72,7 @@ TEST(ContentTest, ECI)
 		c.switchEncoding(ECI::ISO8859_5);
 		c.append(ByteArray{'A', 0xE9, 'Z'});
 		EXPECT_TRUE(c.hasECI);
-		EXPECT_EQ(c.utf16(), L"A\u00E9ZA\u0449Z");
+		EXPECT_EQ(c.utf8(), u8"A\u00E9ZA\u0449Z");
 		EXPECT_EQ(c.bytesECI().asString(), std::string_view("\\000003A\xE9Z\\000007A\xE9Z"));
 	}
 
@@ -81,14 +81,14 @@ TEST(ContentTest, ECI)
 		c.append(ByteArray{'A', 0x83, 0x65, 'Z'});
 		c.switchEncoding(ECI::ISO8859_5);
 		c.append(ByteArray{'A', 0xE9, 'Z'});
-		EXPECT_EQ(c.utf16(), L"A\u0083\u0065ZA\u0449Z");
+		EXPECT_EQ(c.utf8(), u8"A\u0083\u0065ZA\u0449Z");
 		EXPECT_EQ(c.bytesECI().asString(), std::string_view("\\000003A\x83\x65Z\\000007A\xE9Z"));
 	}
 
 	{ // double '\'
 		Content c;
 		c.append("C:\\Test");
-		EXPECT_EQ(c.utf16(), L"C:\\Test");
+		EXPECT_EQ(c.utf8(), u8"C:\\Test");
 		EXPECT_EQ(c.bytesECI().asString(), std::string_view("C:\\\\Test"));
 	}
 }
