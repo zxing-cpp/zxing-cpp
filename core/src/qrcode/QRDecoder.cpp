baffb890f214cba6ba5f0c2d6d63cce8d376b60b
axxel <awagger@gmail.com>
2022-09-20 14:39:15 +0200

    QRCode: transcode std::out_of_range to FormatError
    This fixes #404 and #413. Note: those symbols are both invalid / erroneous
    hence the result has the error flag set and is only reported if
    `DecodeHints::returnErrors` is set. See `-error` option of `ZXingReader`.

--- core/src/qrcode/QRDecoder.cpp
+++ core/src/qrcode/QRDecoder.cpp
@@ -301,6 +301,8 @@ DecoderResult DecodeBitStream(ByteArray&& bytes, const Version& version, ErrorCo
 			}
 			}
 		}
+	} catch (std::out_of_range e) { // see BitSource::readBits
+		error = FormatError("Truncated bit stream");
 	} catch (Error e) {
 		error = std::move(e);
 	}
