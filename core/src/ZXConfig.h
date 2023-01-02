32bdce8bbe200857400df88e579bda16e783c35e
axxel <awagger@gmail.com>
2023-01-02 13:27:09 +0100

    ZXConfig: remove unused #define ZX_HAVE_CONFIG

--- core/src/ZXConfig.h
+++ core/src/ZXConfig.h
@@ -1,17 +1,15 @@
 /*
-* Copyright 2016 Nu-book Inc.
+* Copyright 2018 Axel Waggershauser
 */
 // SPDX-License-Identifier: Apache-2.0
 
 #pragma once
 
-#define ZX_HAVE_CONFIG
-
 // Thread local or static memory may be used to reduce the number of (re-)allocations of temporary variables
 // in e.g. the ReedSolomonDecoder. It is disabled by default. It can be enabled by modifying the following define.
 // Note: The Apple clang compiler until XCode 8 does not support c++11's thread_local.
 // The alternative 'static' makes the code thread unsafe.
-#define ZX_THREAD_LOCAL // 'thread_local' or 'static'
+#define ZX_THREAD_LOCAL // '' (nothing), 'thread_local' or 'static'
 
 // The Galoir Field abstractions used in Reed-Solomon error correction code can use more memory to eliminate a modulo
 // operation. This improves performance but might not be the best option if RAM is scarce. The effect is a few kB big.
