23c19c5f98602a4d69d1667fff99678308b28b5b
liule <levie.liu@gmail.com>
2023-01-06 22:06:24 +0800

    fix crash when the source image is less than 3 pixels width/height

--- core/src/ReadBarcode.cpp
+++ core/src/ReadBarcode.cpp
@@ -76,7 +76,8 @@ public:
 
 		layers.push_back(iv);
 		// TODO: if only matrix codes were considered, then using std::min would be sufficient (see #425)
-		while (threshold > 0 && std::max(layers.back().width(), layers.back().height()) > threshold)
+		while (threshold > 0 && std::max(layers.back().width(), layers.back().height()) > threshold &&
+			   std::min(layers.back().width(), layers.back().height()) >= N)
 			addLayer();
 #if 0
 		// Reversing the layers means we'd start with the smallest. that can make sense if we are only looking for a
