e3c0ecbe7f42bb22e1b24ba6fd56c7bd26b89895
Kien Nguyen <kientux@users.noreply.github.com>
2023-02-02 18:18:06 +0700

    Reduce min iOS deployment target to 11.0

--- wrappers/ios/build-release.sh
+++ wrappers/ios/build-release.sh
@@ -6,7 +6,7 @@ echo ========= Create project structure
 cmake -S../../ -B_builds -GXcode \
     -DCMAKE_SYSTEM_NAME=iOS \
     "-DCMAKE_OSX_ARCHITECTURES=arm64;x86_64" \
-    -DCMAKE_OSX_DEPLOYMENT_TARGET=13.0 \
+    -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0 \
     -DCMAKE_INSTALL_PREFIX=`pwd`/_install \
     -DCMAKE_XCODE_ATTRIBUTE_ONLY_ACTIVE_ARCH=NO \
     -DBUILD_UNIT_TESTS=NO \
