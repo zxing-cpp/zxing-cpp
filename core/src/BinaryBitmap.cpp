zxing-cpp/zxing-cpp

commit 983b3ad187cd2a6ee2bc33b9277e61a80df72eb3

BinaryBitmap: make it possible to call invert() with an empty matrix cache

core/src/BinaryBitmap.cpp
@@ -59,10 +59,8 @@ const BitMatrix* BinaryBitmap::getBitMatrix() const
 
 void BinaryBitmap::invert()
 {
-	if (_cache->matrix) {
-		auto matrix = const_cast<BitMatrix*>(_cache->matrix.get());
+	if (auto matrix = const_cast<BitMatrix*>(getBitMatrix()))
 		matrix->flipAll();
-	}
 	_inverted = true;
 }
 

commit 42a4aa786bbd87e5f9177c0a3f1f126d1ae3d5cc

BinaryBitmap: revert performance regression
In 983b3ad187cd2a6ee2bc33b9277e61a80df72eb3 I did overlook the fact that
it would introduce a mandatory `getBlackMatrix` call even if we were only
looking for linear symbologies.

core/src/BinaryBitmap.cpp
@@ -59,8 +59,10 @@ const BitMatrix* BinaryBitmap::getBitMatrix() const
 
 void BinaryBitmap::invert()
 {
-	if (auto matrix = const_cast<BitMatrix*>(getBitMatrix()))
+	if (_cache->matrix) {
+		auto matrix = const_cast<BitMatrix*>(_cache->matrix.get());
 		matrix->flipAll();
+	}
 	_inverted = true;
 }
 