679b6f60257a33a8aa9b54285eab2b52548345a6
axxel <awagger@gmail.com>
2023-07-14 20:00:32 +0200

    EXPERIMENTAL_API: actually make the new build option work (again...)
    I overlooked another hunk during the commit
    8202a4e13a65de3a2b459d121af7b02cc7d2e9b5. double sigh...

--- core/src/ReadBarcode.cpp
+++ core/src/ReadBarcode.cpp
@@ -146,7 +146,7 @@ Results ReadBarcodes(const ImageView& _iv, const DecodeHints& hints)
 		return {reader.read(*CreateBitmap(hints.binarizer(), iv))};
 
 	std::unique_ptr<MultiFormatReader> closedReader;
-#ifdef BUILD_EXPERIMENTAL_API
+#ifdef ZXING_BUILD_EXPERIMENTAL_API
 	auto formatsBenefittingFromClosing = BarcodeFormat::Aztec | BarcodeFormat::DataMatrix | BarcodeFormat::QRCode | BarcodeFormat::MicroQRCode;
 	if (hints.tryDenoise() && hints.hasFormat(formatsBenefittingFromClosing)) {
 		DecodeHints closedHints = hints;
