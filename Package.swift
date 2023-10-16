8bfca58d0d6bf7228267f8fe83a6e4ad03cf14bf
Ben John <john@liftric.com>
2023-10-16 17:48:02 +0200

    fix(wrappers/ios): omit the type (static vs dynamic) for the library in swift pm - without the `type` attribute, it gets either linked statically or dynamically depending on the target

--- Package.swift
+++ Package.swift
@@ -9,7 +9,6 @@ let package = Package(
     products: [
         .library(
             name: "ZXingCppWrapper",
-            type: .dynamic,
             targets: ["ZXingCppWrapper"])
     ],
     targets: [
