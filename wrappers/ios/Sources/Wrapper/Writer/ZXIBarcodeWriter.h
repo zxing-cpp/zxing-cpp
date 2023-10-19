bf06236cdec02ae3a817417f2bb86d0f830b98cc
Alex Manzer <alex@manzer.de>
2023-10-19 10:24:24 +0200

    iOS wrapper: Add functionality of encoding binary data into Barcodes (#635)
    * Rename methods to writeText and writeBytes
    
    ---------
    
    Co-authored-by: Alexander Manzer <alexander.manzer@kurzdigital.com>

--- wrappers/ios/Sources/Wrapper/Writer/ZXIBarcodeWriter.h
+++ wrappers/ios/Sources/Wrapper/Writer/ZXIBarcodeWriter.h
@@ -9,11 +9,18 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface ZXIBarcodeWriter : NSObject
 
--(nullable CGImageRef)write:(NSString *)contents
-                      width:(int)width
-                     height:(int)height
-                     format:(ZXIFormat)format
-                      error:(NSError **)error;
+-(nullable CGImageRef)writeText:(NSString *)contents
+                          width:(int)width
+                         height:(int)height
+                         format:(ZXIFormat)format
+                          error:(NSError *__autoreleasing  _Nullable *)error;
+
+-(nullable CGImageRef)writeBytes:(NSData *)data
+                           width:(int)width
+                          height:(int)height
+                          format:(ZXIFormat)format
+                           error:(NSError *__autoreleasing  _Nullable *)error;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- wrappers/ios/Sources/Wrapper/Writer/ZXIBarcodeWriter.mm
+++ wrappers/ios/Sources/Wrapper/Writer/ZXIBarcodeWriter.mm
@@ -6,6 +6,7 @@
 #import "ZXIBarcodeWriter.h"
 #import "MultiFormatWriter.h"
 #import "BitMatrix.h"
+#import "BitMatrixIO.h"
 #import "ZXIFormatHelper.h"
 #import "ZXIErrors.h"
 #import <iostream>
@@ -18,69 +19,58 @@ std::wstring NSStringToStringW(NSString* str) {
                         sizeof(wchar_t));
 }
 
-#ifdef  DEBUG
-std::string ToString(const BitMatrix& matrix, char one, char zero, bool addSpace, bool printAsCString)
-{
-    std::string result;
-    result.reserve((addSpace ? 2 : 1) * (matrix.width() * matrix.height()) + matrix.height());
-    for (int y = 0; y < matrix.height(); ++y) {
-        for (int x = 0; x < matrix.width(); ++x) {
-            result += matrix.get(x, y) ? one : zero;
-            if (addSpace)
-                result += ' ';
-        }
-        if (printAsCString)
-            result += "\\n\"";
-        result += '\n';
+std::wstring NSDataToStringW(NSData *data) {
+    std::wstring s;
+    const unsigned char *bytes = (const unsigned char *) [data bytes];
+    size_t len = [data length];
+	for (int i = 0; i < len; ++i) {
+        s.push_back(bytes[i]);
     }
-    return result;
+    return s;
 }
-#endif
 
 @implementation ZXIBarcodeWriter
 
--(CGImageRef)write:(NSString *)contents
-             width:(int)width
-            height:(int)height
-            format:(ZXIFormat)format
-             error:(NSError *__autoreleasing  _Nullable *)error {
+-(CGImageRef)writeBytes:(NSData *)data
+                  width:(int)width
+                 height:(int)height
+                 format:(ZXIFormat)format
+                  error:(NSError *__autoreleasing  _Nullable *)error {
+    return [self encode: NSDataToStringW(data)
+                  width: width
+                 height: height
+                 format: format
+               encoding: CharacterSet::BINARY
+                  error: error];
+}
+
+-(CGImageRef)writeText:(NSString *)contents
+                 width:(int)width
+                height:(int)height
+                format:(ZXIFormat)format
+                 error:(NSError *__autoreleasing  _Nullable *)error {
+    return [self encode: NSStringToStringW(contents)
+                  width: width
+                 height: height
+                 format: format
+               encoding: CharacterSet::UTF8
+                  error: error];
+}
+
+-(CGImageRef)encode:(std::wstring)content
+              width:(int)width
+             height:(int)height
+             format:(ZXIFormat)format
+           encoding:(CharacterSet)encoding
+              error:(NSError *__autoreleasing  _Nullable *)error {
     MultiFormatWriter writer { BarcodeFormatFromZXIFormat(format) };
+    writer.setEncoding(encoding);
     // Catch exception for invalid formats
     try {
-        BitMatrix result = writer.encode(NSStringToStringW(contents), width, height);
-        int realWidth = result.width();
-        int realHeight = result.height();
-
-#ifdef DEBUG
-//        std::cout << ToString(result, 'X', ' ', false, false);
-#endif
-
-        NSMutableData *resultAsNSData = [[NSMutableData alloc] initWithLength:realWidth * realHeight];
-        size_t index = 0;
-        uint8_t *bytes = (uint8_t*)resultAsNSData.mutableBytes;
-        for (int y = 0; y < realHeight; ++y) {
-            for (int x = 0; x < realWidth; ++x) {
-                bytes[index] = result.get(x, y) ? 0 : 255;
-                ++index;
-            }
-        }
-
-        CGColorSpaceRef colorSpace = CGColorSpaceCreateWithName(kCGColorSpaceGenericGray);
-
-        CGImageRef cgimage = CGImageCreate(realWidth,
-                                           realHeight,
-                                           8,
-                                           8,
-                                           realWidth,
-                                           colorSpace,
-                                           kCGBitmapByteOrderDefault,
-                                           CGDataProviderCreateWithCFData((CFDataRef)resultAsNSData),
-                                           NULL,
-                                           YES,
-                                           kCGRenderingIntentDefault);
-        return cgimage;
+        BitMatrix bitMatrix = writer.encode(content, width, height);
+        return [self inflate:&bitMatrix];
     } catch(std::exception &e) {
-        if(error != nil) {
+        if (error != nil) {
             NSDictionary *userInfo = @{
                 NSLocalizedDescriptionKey: [[NSString alloc] initWithUTF8String:e.what()]
             };
@@ -88,6 +78,39 @@ std::string ToString(const BitMatrix& matrix, char one, char zero, bool addSpace
         }
         return nil;
     }
+}
+
+-(CGImageRef)inflate:(BitMatrix *)bitMatrix {
+    int realWidth = bitMatrix->width();
+    int realHeight = bitMatrix->height();
+
+#ifdef DEBUG
+    std::cout << ToString(*bitMatrix, 'X', ' ', false, false);
+#endif
+
+    NSMutableData *resultAsNSData = [[NSMutableData alloc] initWithLength:realWidth * realHeight];
+    size_t index = 0;
+    uint8_t *bytes = (uint8_t*)resultAsNSData.mutableBytes;
+    for (int y = 0; y < realHeight; ++y) {
+        for (int x = 0; x < realWidth; ++x) {
+            bytes[index] = bitMatrix->get(x, y) ? 0 : 255;
+            ++index;
+        }
+    }
+
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateWithName(kCGColorSpaceGenericGray);
+
+    return CGImageCreate(realWidth,
+                         realHeight,
+                         8,
+                         8,
+                         realWidth,
+                         colorSpace,
+                         kCGBitmapByteOrderDefault,
+                         CGDataProviderCreateWithCFData((CFDataRef)resultAsNSData),
+                         NULL,
+                         YES,
+                         kCGRenderingIntentDefault);
 }
 
 @end
