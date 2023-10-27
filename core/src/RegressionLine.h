zxing-cpp/zxing-cpp

commit 9d40fbdac9bdc37ce58ab803f20699636fd8c60b

DataMatrix: tune line tracing and pure vs non-pure detection order
This fixes the issue discussed in https://github.com/markusfisch/zxing-cpp/commit/850243cb3b5572478b39e52f35cc6d0bd0d0de5f#r130276661

core/src/datamatrix/DMDetector.cpp
@@ -552,21 +552,24 @@ public:
 		return true;
 	}
 
+	bool updateDirectionFromLine(RegressionLine& line)
+	{
+		return line.evaluate(1.5) && updateDirectionFromOrigin(p - line.project(p) + line.points().front());
+	}
+
 	bool traceLine(PointF dEdge, RegressionLine& line)
 	{
 		line.setDirectionInward(dEdge);
 		do {
 			log(p);
 			line.add(p);
-			if (line.points().size() % 50 == 10) {
-				if (!line.evaluate())
-					return false;
-				if (!updateDirectionFromOrigin(p - line.project(p) + line.points().front()))
-					return false;
-			}
+			if (line.points().size() % 50 == 10 && !updateDirectionFromLine(line))
+				return false;
 			auto stepResult = traceStep(dEdge, 1, line.isValid());
-			if (stepResult != StepResult::FOUND)
+			if (stepResult != StepResult::FOUND) {
+				updateDirectionFromLine(line);
 				return stepResult == StepResult::OPEN_END && line.points().size() > 1;
+			}
 		} while (true);
 	}
 
@@ -618,9 +621,7 @@ public:
 				if (stepLengthInMainDir > 1 || maxAbsComponent(curStep) >= 2) {
 					++gaps;
 					if (gaps >= 2 || line.points().size() > 5) {
-						if (!line.evaluate(1.5))
-							return false;
-						if (!updateDirectionFromOrigin(p - line.project(p) + line.points().front()))
+						if (!updateDirectionFromLine(line))
 							return false;
 						// check if the first half of the top-line trace is complete.
 						// the minimum code size is 10x10 -> every code has at least 4 gaps
@@ -919,31 +920,26 @@ static DetectorResult DetectPure(const BitMatrix& image)
 DetectorResults Detect(const BitMatrix& image, bool tryHarder, bool tryRotate, bool isPure)
 {
 #ifdef __cpp_impl_coroutine
-	if (isPure) {
-		if (auto r = DetectPure(image); r.isValid())
-			co_yield std::move(r);
-	} else {
+	// First try the very fast DetectPure() path. Also because DetectNew() generally fails with pure module size 1 symbols
+	// TODO: implement a tryRotate version of DetectPure, see #590.
+	if (auto r = DetectPure(image); r.isValid())
+		co_yield std::move(r);
+	else if (!isPure) { // If r.isValid() then there is no point in looking for more (no-pure) symbols
 		bool found = false;
 		for (auto&& r : DetectNew(image, tryHarder, tryRotate)) {
 			found = true;
 			co_yield std::move(r);
 		}
 		if (!found && tryHarder) {
-			//TODO: implement a tryRotate version of DetectPure, see #590.
-			if (auto r = DetectPure(image); r.isValid())
-				co_yield std::move(r);
-			else if(auto r = DetectOld(image); r.isValid())
+			if (auto r = DetectOld(image); r.isValid())
 				co_yield std::move(r);
 		}
 	}
 #else
-	if (isPure)
-		return DetectPure(image);
-
-	auto result = DetectNew(image, tryHarder, tryRotate);
-	if (!result.isValid() && tryHarder)
-		result = DetectPure(image);
-	if (!result.isValid() && tryHarder)
+	auto result = DetectPure(image);
+	if (!result.isValid() && !isPure)
+		result = DetectNew(image, tryHarder, tryRotate);
+	if (!result.isValid() && tryHarder && !isPure)
 		result = DetectOld(image);
 	return result;
 #endif

test/blackbox/BlackboxTestRunner.cpp
@@ -348,7 +348,7 @@ int runBlackBoxTests(const fs::path& testPathPrefix, const std::set<std::string>
 		});
 
 		runTests("datamatrix-1", "DataMatrix", 29, {
-			{ 27, 29, 0   },
+			{ 29, 29, 0   },
 			{  0, 27, 90  },
 			{  0, 27, 180 },
 			{  0, 27, 270 },

commit f0211a481d7f0a32593d75ca8895d8ec9f91e62d

DMDetector: improve performance
This fixes a performance regression introduced in
9d40fbdac9bdc37ce58ab803f20699636fd8c60b

core/src/RegressionLine.h
@@ -79,6 +79,7 @@ public:
 	auto signedDistance(PointF p) const { return dot(normal(), p) - c; }
 	template <typename T> auto distance(PointT<T> p) const { return std::abs(signedDistance(PointF(p))); }
 	PointF project(PointF p) const { return p - signedDistance(p) * normal(); }
+	PointF centroid() const { return std::accumulate(_points.begin(), _points.end(), PointF()) / _points.size(); }
 
 	void reset()
 	{
@@ -119,7 +120,7 @@ public:
 				if (old_points_size == points.size())
 					break;
 #ifdef PRINT_DEBUG
-				printf("removed %zu points\n", old_points_size - points.size());
+				printf("removed %zu points -> %zu remaining\n", old_points_size - points.size(), points.size());
 #endif
 				ret = evaluate(points);
 			}

core/src/datamatrix/DMDetector.cpp
@@ -557,19 +557,23 @@ public:
 		return line.evaluate(1.5) && updateDirectionFromOrigin(p - line.project(p) + line.points().front());
 	}
 
+	bool updateDirectionFromLineCentroid(RegressionLine& line)
+	{
+		// Basically a faster, less accurate version of the above without the line evaluation
+		return updateDirectionFromOrigin(line.centroid());
+	}
+
 	bool traceLine(PointF dEdge, RegressionLine& line)
 	{
 		line.setDirectionInward(dEdge);
 		do {
 			log(p);
 			line.add(p);
-			if (line.points().size() % 50 == 10 && !updateDirectionFromLine(line))
+			if (line.points().size() % 50 == 10 && !updateDirectionFromLineCentroid(line))
 				return false;
 			auto stepResult = traceStep(dEdge, 1, line.isValid());
-			if (stepResult != StepResult::FOUND) {
-				updateDirectionFromLine(line);
-				return stepResult == StepResult::OPEN_END && line.points().size() > 1;
-			}
+			if (stepResult != StepResult::FOUND)
+				return stepResult == StepResult::OPEN_END && line.points().size() > 1 && updateDirectionFromLineCentroid(line);
 		} while (true);
 	}
 