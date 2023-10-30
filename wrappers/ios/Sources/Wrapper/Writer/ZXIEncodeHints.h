cb21a92fb37cc2396ff1ba8506d18277f603f876
Markus Fisch <markus.fisch@kurzdigital.com>
2023-10-30 12:02:17 +0100

    ios: fix declaring error correction constants
    Separate declaration and initialization into header and implemenation
    to avoid duplicated symbols when the header file is included multiple
    times.

--- wrappers/ios/Sources/Wrapper/Writer/ZXIEncodeHints.h
+++ wrappers/ios/Sources/Wrapper/Writer/ZXIEncodeHints.h
@@ -7,6 +7,29 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+extern const int AZTEC_ERROR_CORRECTION_0;
+extern const int AZTEC_ERROR_CORRECTION_12;
+extern const int AZTEC_ERROR_CORRECTION_25;
+extern const int AZTEC_ERROR_CORRECTION_37;
+extern const int AZTEC_ERROR_CORRECTION_50;
+extern const int AZTEC_ERROR_CORRECTION_62;
+extern const int AZTEC_ERROR_CORRECTION_75;
+extern const int AZTEC_ERROR_CORRECTION_87;
+extern const int AZTEC_ERROR_CORRECTION_100;
+extern const int QR_ERROR_CORRECTION_LOW;
+extern const int QR_ERROR_CORRECTION_MEDIUM;
+extern const int QR_ERROR_CORRECTION_QUARTILE;
+extern const int QR_ERROR_CORRECTION_HIGH;
+extern const int PDF417_ERROR_CORRECTION_0;
+extern const int PDF417_ERROR_CORRECTION_1;
+extern const int PDF417_ERROR_CORRECTION_2;
+extern const int PDF417_ERROR_CORRECTION_3;
+extern const int PDF417_ERROR_CORRECTION_4;
+extern const int PDF417_ERROR_CORRECTION_5;
+extern const int PDF417_ERROR_CORRECTION_6;
+extern const int PDF417_ERROR_CORRECTION_7;
+extern const int PDF417_ERROR_CORRECTION_8;
+
 @interface ZXIEncodeHints : NSObject
 @property(nonatomic) ZXIFormat format;
 @property(nonatomic) int width;
