cdb921ea3220f654fd920012eff183678e42c71c
Markus Fisch <markus.fisch@kurzdigital.com>
2023-10-31 15:34:01 +0100

    ios: clean up ZXIDecodeHints
    And match argument order with native DecodeHints.

--- wrappers/ios/Sources/Wrapper/Reader/ZXIDecodeHints.h
+++ wrappers/ios/Sources/Wrapper/Reader/ZXIDecodeHints.h
@@ -9,28 +9,27 @@ NS_ASSUME_NONNULL_BEGIN
 @interface ZXIDecodeHints : NSObject
 @property(nonatomic) BOOL tryHarder;
 @property(nonatomic) BOOL tryRotate;
-@property(nonatomic) BOOL tryDownscale;
 @property(nonatomic) BOOL tryInvert;
+@property(nonatomic) BOOL tryDownscale;
 @property(nonatomic) BOOL tryCode39ExtendedMode;
 @property(nonatomic) BOOL validateCode39CheckSum;
 @property(nonatomic) BOOL validateITFCheckSum;
 @property(nonatomic) uint8_t downscaleFactor;
 @property(nonatomic) uint16_t downscaleThreshold;
-
 @property(nonatomic) NSInteger maxNumberOfSymbols;
 /// An array of ZXIFormat
 @property(nonatomic, strong) NSArray<NSNumber*> *formats;
 
 - (instancetype)initWithTryHarder:(BOOL)tryHarder
                         tryRotate:(BOOL)tryRotate
-                     tryDownscale:(BOOL)tryDownscale
-               maxNumberOfSymbols:(NSInteger)maxNumberOfSymbols
                         tryInvert:(BOOL)tryInvert
+                     tryDownscale:(BOOL)tryDownscale
             tryCode39ExtendedMode:(BOOL)tryCode39ExtendedMode
            validateCode39CheckSum:(BOOL)validateCode39CheckSum
               validateITFCheckSum:(BOOL)validateITFCheckSum
                   downscaleFactor:(uint8_t)downscaleFactor
                downscaleThreshold:(uint16_t)downscaleThreshold
+               maxNumberOfSymbols:(NSInteger)maxNumberOfSymbols
                           formats:(NSArray<NSNumber*>*)formats;
 @end
 
