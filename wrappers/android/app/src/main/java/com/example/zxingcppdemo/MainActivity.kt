2d0e2713b73d6e67129cc5ccfcdf33f558ce9f90
Markus Fisch <markus.fisch@kurzdigital.com>
2023-11-08 15:27:36 +0100

    android: remove WRITE_EXTERNAL_STORAGE for R+
    Requesting WRITE_EXTERNAL_STORAGE on Android R (SDK 30) *always* fails
    and so the sample app is always immediately closed.
    
    This permission has no effect on R+. See:
    https://developer.android.com/reference/android/Manifest.permission#WRITE_EXTERNAL_STORAGE
    
    To save anything in external storage, this sample would have to be
    migrated to Scoped Storage.

--- wrappers/android/app/src/main/java/com/example/zxingcppdemo/MainActivity.kt
+++ wrappers/android/app/src/main/java/com/example/zxingcppdemo/MainActivity.kt
@@ -24,6 +24,7 @@ import android.hardware.camera2.CaptureRequest
 import android.media.AudioManager
 import android.media.MediaActionSound
 import android.media.ToneGenerator
+import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.view.View
@@ -51,12 +52,20 @@ class MainActivity : AppCompatActivity() {
 	private lateinit var binding: ActivityCameraBinding
 
 	private val executor = Executors.newSingleThreadExecutor()
-	private val permissions = listOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+	private val permissions = mutableListOf(Manifest.permission.CAMERA)
 	private val permissionsRequestCode = Random.nextInt(0, 10000)
 
 	private val beeper = ToneGenerator(AudioManager.STREAM_NOTIFICATION, 50)
 	private var lastText = String()
 	private var doSaveImage: Boolean = false
+
+	init {
+		// On R or higher, this permission has no effect. See:
+		// https://developer.android.com/reference/android/Manifest.permission#WRITE_EXTERNAL_STORAGE
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
+			permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+		}
+	}
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
