cf596b066c9c356f745c2be809ce5454b1b39d2e
Markus Fisch <markus.fisch@kurzdigital.com>
2023-11-10 13:37:16 +0100

    android: reformat library source
    To use tabs for indentation like the sample app does.
    
    There's no point in using a different indentation here.

--- wrappers/android/zxingcpp/src/main/java/com/zxingcpp/ZXingCpp.kt
+++ wrappers/android/zxingcpp/src/main/java/com/zxingcpp/ZXingCpp.kt
@@ -22,121 +22,131 @@ import android.graphics.Point
 import android.graphics.Rect
 import android.os.Build
 import androidx.camera.core.ImageProxy
-import java.lang.RuntimeException
 import java.nio.ByteBuffer
 
 public object ZXingCpp {
-    private val supportedYUVFormats: List<Int> =
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            listOf(ImageFormat.YUV_420_888, ImageFormat.YUV_422_888, ImageFormat.YUV_444_888)
-        } else {
-            listOf(ImageFormat.YUV_420_888)
-        }
+	private val supportedYUVFormats: List<Int> =
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+			listOf(ImageFormat.YUV_420_888, ImageFormat.YUV_422_888, ImageFormat.YUV_444_888)
+		} else {
+			listOf(ImageFormat.YUV_420_888)
+		}
 
-    init {
-        System.loadLibrary("zxing_android")
-    }
+	init {
+		System.loadLibrary("zxing_android")
+	}
 
-    // Enumerates barcode formats known to this package.
-    // Note that this has to be kept synchronized with native (C++/JNI) side.
-    public enum class Format {
-        NONE, AZTEC, CODABAR, CODE_39, CODE_93, CODE_128, DATA_BAR, DATA_BAR_EXPANDED,
-        DATA_MATRIX, EAN_8, EAN_13, ITF, MAXICODE, PDF_417, QR_CODE, MICRO_QR_CODE, UPC_A, UPC_E
-    }
+	// Enumerates barcode formats known to this package.
+	// Note that this has to be kept synchronized with native (C++/JNI) side.
+	public enum class Format {
+		NONE, AZTEC, CODABAR, CODE_39, CODE_93, CODE_128, DATA_BAR, DATA_BAR_EXPANDED,
+		DATA_MATRIX, EAN_8, EAN_13, ITF, MAXICODE, PDF_417, QR_CODE, MICRO_QR_CODE, UPC_A, UPC_E
+	}
 
-    public enum class ContentType {
-        TEXT, BINARY, MIXED, GS1, ISO15434, UNKNOWN_ECI
-    }
+	public enum class ContentType {
+		TEXT, BINARY, MIXED, GS1, ISO15434, UNKNOWN_ECI
+	}
 
-    public enum class Binarizer {
-        LOCAL_AVERAGE, GLOBAL_HISTOGRAM, FIXED_THRESHOLD, BOOL_CAST
-    }
+	public enum class Binarizer {
+		LOCAL_AVERAGE, GLOBAL_HISTOGRAM, FIXED_THRESHOLD, BOOL_CAST
+	}
 
-    public enum class EanAddOnSymbol {
-        IGNORE, READ, REQUIRE
-    }
+	public enum class EanAddOnSymbol {
+		IGNORE, READ, REQUIRE
+	}
 
-    public enum class TextMode {
-        PLAIN, ECI, HRI, HEX, ESCAPED
-    }
+	public enum class TextMode {
+		PLAIN, ECI, HRI, HEX, ESCAPED
+	}
 
-    public data class DecodeHints(
-        val formats: Set<Format> = setOf(),
-        val tryHarder: Boolean = false,
-        val tryRotate: Boolean = false,
-        val tryInvert: Boolean = false,
-        val tryDownscale: Boolean = false,
-        val isPure: Boolean = false,
-        val tryCode39ExtendedMode: Boolean = false,
-        val validateCode39CheckSum: Boolean = false,
-        val validateITFCheckSum: Boolean = false,
-        val returnCodabarStartEnd: Boolean = false,
-        val returnErrors: Boolean = false,
-        val downscaleFactor: Int = 3,
-        val eanAddOnSymbol: EanAddOnSymbol = EanAddOnSymbol.IGNORE,
-        val binarizer: Binarizer = Binarizer.LOCAL_AVERAGE,
-        val textMode: TextMode = TextMode.HRI,
-        val minLineCount: Int = 2,
-        val maxNumberOfSymbols: Int = 0xff,
-        val downscaleThreshold: Int = 500
-    )
+	public data class DecodeHints(
+		val formats: Set<Format> = setOf(),
+		val tryHarder: Boolean = false,
+		val tryRotate: Boolean = false,
+		val tryInvert: Boolean = false,
+		val tryDownscale: Boolean = false,
+		val isPure: Boolean = false,
+		val tryCode39ExtendedMode: Boolean = false,
+		val validateCode39CheckSum: Boolean = false,
+		val validateITFCheckSum: Boolean = false,
+		val returnCodabarStartEnd: Boolean = false,
+		val returnErrors: Boolean = false,
+		val downscaleFactor: Int = 3,
+		val eanAddOnSymbol: EanAddOnSymbol = EanAddOnSymbol.IGNORE,
+		val binarizer: Binarizer = Binarizer.LOCAL_AVERAGE,
+		val textMode: TextMode = TextMode.HRI,
+		val minLineCount: Int = 2,
+		val maxNumberOfSymbols: Int = 0xff,
+		val downscaleThreshold: Int = 500
+	)
 
-    public data class Position(
-        val topLeft: Point,
-        val topRight: Point,
-        val bottomLeft: Point,
-        val bottomRight: Point,
-        val orientation: Double
-    )
+	public data class Position(
+		val topLeft: Point,
+		val topRight: Point,
+		val bottomLeft: Point,
+		val bottomRight: Point,
+		val orientation: Double
+	)
 
-    public data class Result(
-        val format: Format,
-        val bytes: ByteArray?,
-        val text: String?,
-        val time: String?, // for development/debug purposes only
-        val contentType: ContentType,
-        val position: Position?,
-        val orientation: Int,
-        val ecLevel: String?,
-        val symbologyIdentifier: String?,
-        val sequenceSize: Int,
-        val sequenceIndex: Int,
-        val sequenceId: String,
-        val readerInit: Boolean,
-        val lineCount: Int
-    )
+	public data class Result(
+		val format: Format,
+		val bytes: ByteArray?,
+		val text: String?,
+		val time: String?, // for development/debug purposes only
+		val contentType: ContentType,
+		val position: Position?,
+		val orientation: Int,
+		val ecLevel: String?,
+		val symbologyIdentifier: String?,
+		val sequenceSize: Int,
+		val sequenceIndex: Int,
+		val sequenceId: String,
+		val readerInit: Boolean,
+		val lineCount: Int
+	)
 
-    public fun read(image: ImageProxy, decodeHints: DecodeHints): List<Result>? {
-        check(image.format in supportedYUVFormats) {
-            "Invalid image format: ${image.format}. Must be one of: $supportedYUVFormats"
-        }
+	public fun read(image: ImageProxy, decodeHints: DecodeHints): List<Result>? {
+		check(image.format in supportedYUVFormats) {
+			"Invalid image format: ${image.format}. Must be one of: $supportedYUVFormats"
+		}
 
-        return readYBuffer(
-            image.planes[0].buffer,
-            image.planes[0].rowStride,
-            image.cropRect.left,
-            image.cropRect.top,
-            image.cropRect.width(),
-            image.cropRect.height(),
-            image.imageInfo.rotationDegrees,
-            decodeHints
-        )
-    }
+		return readYBuffer(
+			image.planes[0].buffer,
+			image.planes[0].rowStride,
+			image.cropRect.left,
+			image.cropRect.top,
+			image.cropRect.width(),
+			image.cropRect.height(),
+			image.imageInfo.rotationDegrees,
+			decodeHints
+		)
+	}
 
-    public fun read(bitmap: Bitmap, decodeHints: DecodeHints, cropRect: Rect = Rect(), rotation: Int = 0): List<Result>? {
-        return readBitmap(
-            bitmap, cropRect.left, cropRect.top, cropRect.width(), cropRect.height(), rotation,
-            decodeHints
-        )
-    }
+	public fun read(
+		bitmap: Bitmap,
+		decodeHints: DecodeHints,
+		cropRect: Rect = Rect(),
+		rotation: Int = 0
+	): List<Result>? {
+		return readBitmap(
+			bitmap, cropRect.left, cropRect.top, cropRect.width(), cropRect.height(), rotation,
+			decodeHints
+		)
+	}
 
-    private external fun readYBuffer(
-        yBuffer: ByteBuffer, rowStride: Int, left: Int, top: Int, width: Int, height: Int, rotation: Int,
-        decodeHints: DecodeHints
-    ): List<Result>?
+	private external fun readYBuffer(
+		yBuffer: ByteBuffer,
+		rowStride: Int,
+		left: Int,
+		top: Int,
+		width: Int,
+		height: Int,
+		rotation: Int,
+		decodeHints: DecodeHints
+	): List<Result>?
 
-    private external fun readBitmap(
-        bitmap: Bitmap, left: Int, top: Int, width: Int, height: Int, rotation: Int,
-        decodeHints: DecodeHints
-    ): List<Result>?
+	private external fun readBitmap(
+		bitmap: Bitmap, left: Int, top: Int, width: Int, height: Int, rotation: Int,
+		decodeHints: DecodeHints
+	): List<Result>?
 }
