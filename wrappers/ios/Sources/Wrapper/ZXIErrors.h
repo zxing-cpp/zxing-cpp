2ef3267f156cd97718364b78b065e4d81d18f3e6
Markus Fisch <markus.fisch@kurzdigital.com>
2023-11-17 17:22:46 +0100

    ios: improve exception/error handling
    Make `read()` throw so C++ exceptions can be caught in Swift.
    
    So an app can't crash silently anymore when a C++ exception is thrown
    while trying to read a barcode.
    
    This also adds a function to properly set a NSError from a C++
    exception to avoid code duplication in ZXIBarcodeReader/Writer.

--- wrappers/ios/Sources/Wrapper/ZXIErrors.h
+++ wrappers/ios/Sources/Wrapper/ZXIErrors.h
@@ -8,8 +8,14 @@ NS_ASSUME_NONNULL_BEGIN
 
 #define ZXIErrorDomain @"ZXIErrorDomain"
 
+typedef NS_ENUM(NSInteger, ZXIBarcodeReaderError) {
+    ZXIReaderError,
+};
+
 typedef NS_ENUM(NSInteger, ZXIBarcodeWriterError) {
     ZXIWriterError,
 };
+
+void SetNSError(NSError *__autoreleasing _Nullable* error, NSInteger code, const char* message);
 
 NS_ASSUME_NONNULL_END
