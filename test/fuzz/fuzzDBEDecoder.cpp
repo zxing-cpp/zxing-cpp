e587383783203ebe8e5683996a9ce991d7c6e540
axxel <awagger@gmail.com>
2023-12-05 00:22:22 +0100

    fuzz: fix/update fuzzer code

--- test/fuzz/fuzzDBEDecoder.cpp
+++ test/fuzz/fuzzDBEDecoder.cpp
@@ -7,6 +7,7 @@
 #include <stddef.h>
 
 #include "BitArray.h"
+#include "Error.h"
 #include "oned/ODDataBarExpandedBitDecoder.h"
 
 using namespace ZXing;
@@ -20,7 +21,11 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
 	for (size_t i = 0; i < size; ++i)
 		bits.appendBits(data[i], 8);
 
-	OneD::DataBar::DecodeExpandedBits(bits);
+	try {
+		OneD::DataBar::DecodeExpandedBits(bits);
+	} catch (std::out_of_range) {
+	} catch (Error) {
+	}
 
 	return 0;
 }
--- test/fuzz/fuzzODDecoders.cpp
+++ test/fuzz/fuzzODDecoders.cpp
@@ -24,7 +24,7 @@ static std::vector<std::unique_ptr<RowReader>> readers;
 
 bool init()
 {
-	DecodeHints hints;
+	static DecodeHints hints;
 	readers.emplace_back(new MultiUPCEANReader(hints));
 	readers.emplace_back(new Code39Reader(hints));
 	readers.emplace_back(new Code93Reader(hints));
