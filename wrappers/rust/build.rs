bc22603796ce074bc7647296f913499137767fdb
Steve Cook <steviecook210@gmail.com>
2024-01-08 16:01:26 -0500

    Add simple README.md

--- wrappers/rust/build.rs
+++ wrappers/rust/build.rs
@@ -3,11 +3,14 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() -> miette::Result<()> {
+    // Environment variable used to search for the ZXing CPP static library
+    // This is only necessary for binaries during the linking stage
     if let Ok(lib_dir) = env::var("ZXING_CPP_LIB_DIR") {
         println!("cargo:rustc-link-search=native={}", lib_dir);
         println!("cargo:rustc-link-lib=static=ZXing");
     }
 
+    // Required environment variable used to build bindings with autocxx
     let src_path: PathBuf = env::var("ZXING_CPP_SRC_DIR")
         .into_diagnostic()
         .map_err(|e| e.context("ZXING_CPP_SRC_DIR"))?
