aba16d0d8406ad637035d239eba85a043946379e
ISNing <ISNing_SF@outlook.com>
2024-02-10 17:08:20 +0800

    kn: BarcodeReader: Correctly handle `zxing_ReadBarcodes` returning null

--- wrappers/kn/src/nativeMain/kotlin/zxingcpp/BarcodeReader.kt
+++ wrappers/kn/src/nativeMain/kotlin/zxingcpp/BarcodeReader.kt
@@ -1,13 +1,18 @@
 package zxingcpp
 
 import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.toKString
+import zxingcpp.cinterop.zxing_LastErrorMsg
 import zxingcpp.cinterop.zxing_ReadBarcodes
 
 @OptIn(ExperimentalForeignApi::class, ExperimentalStdlibApi::class)
 class BarcodeReader(val options: ReaderOptions? = null) {
+	@Throws(BarcodeReadingException::class)
 	fun read(imageView: ImageView): List<Barcode> =
 		imageView.cValueWrapped.use {
 			zxing_ReadBarcodes(it.cValue, options?.cValue)?.toKObject()
-				?: error("zxing_ReadBarcodes returned null, which is an unexpected behaviour")
+				?: throw BarcodeReadingException(zxing_LastErrorMsg()?.toKString())
 		}
 }
+
+class BarcodeReadingException(message: String?) : Exception("Failed to read barcodes: $message")
