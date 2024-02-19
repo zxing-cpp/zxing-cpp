55ca66f3609551f4e8bf3e9731e47aaacf3c3b0e
ISNing <ISNing_SF@outlook.com>
2024-02-19 17:47:32 +0800

    kn: Update copyrights

--- wrappers/kn/src/nativeMain/kotlin/zxingcpp/BarcodeFormat.kt
+++ wrappers/kn/src/nativeMain/kotlin/zxingcpp/BarcodeFormat.kt
@@ -1,3 +1,8 @@
+/*
+* Copyright 2024 ISNing
+*/
+// SPDX-License-Identifier: Apache-2.0
+
 package zxingcpp
 
 import kotlinx.cinterop.ExperimentalForeignApi
--- wrappers/kn/src/nativeTest/kotlin/Test.kt
+++ wrappers/kn/src/nativeTest/kotlin/Test.kt
@@ -1,3 +1,8 @@
+/*
+* Copyright 2024 ISNing
+*/
+// SPDX-License-Identifier: Apache-2.0
+
 import zxingcpp.*
 import kotlin.experimental.ExperimentalNativeApi
 import kotlin.test.Test
