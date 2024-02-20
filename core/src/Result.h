df191aa59cd06efd8c5351baf0221b305bc71e9a
axxel <awagger@gmail.com>
2024-02-20 13:15:07 +0100

    ci: fix build regression (forgot to include deprecated header `Result.h)

---
+++ core/src/Result.h
@@ -0,0 +1,10 @@
+/*
+* Copyright 2024 Axel Waggershauser
+*/
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "Barcode.h"
+
+#pragma message("Header `Result.h` is deprecated, please include `Barcode.h` and use the new name `Barcode`.")
