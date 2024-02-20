588fa49f7cb45ff8d68166db6a36c5b3fd251c63
axxel <awagger@gmail.com>
2024-02-20 18:18:05 +0100

    ci: fix the problem with the wrong python version in the macOS wheels
    See #695. This changeset hopefully fixed both issues:
     * module compiled for the wrong python version
     * c++20 support for macOS
    
    Also: actually do the upload-artifacts@v4 update and drop the pre-build
    binary support for Python 3.8.

--- wrappers/python/setup.py
+++ wrappers/python/setup.py
@@ -18,7 +18,7 @@ class CMakeBuild(build_ext):
 	def build_extension(self, ext):
 		extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
 		cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-					  '-DPYTHON_EXECUTABLE=' + sys.executable,
+					  '-DPython3_EXECUTABLE=' + sys.executable,
 					  '-DVERSION_INFO=' + self.distribution.get_version()]
 
 		cfg = 'Debug' if self.debug else 'Release'
