68036083a231c065cdeaedee2488341c9d488f5f
axxel <awagger@gmail.com>
2024-03-19 21:29:26 +0100

    HRI: strip a few kB of binary size

--- core/src/HRI.cpp
+++ core/src/HRI.cpp
@@ -10,23 +10,25 @@
 #include "ZXAlgorithms.h"
 
 #include <cmath>
-#include <sstream>
+#include <cstring>
+#include <string_view>
 
 namespace ZXing {
 
 struct AiInfo
 {
-	std::string_view aiPrefix;
-	int _fieldSize;	// if negative, the length is variable and abs(length) give the max size
+	const char aiPrefix[5];
+	int8_t _fieldSize;	// if negative, the length is variable and abs(length) give the max size
 
 	bool isVariableLength() const noexcept { return _fieldSize < 0; }
 	int fieldSize() const noexcept { return std::abs(_fieldSize); }
 	int aiSize() const
 	{
-		if ((aiPrefix[0] == '3' && Contains("1234569", aiPrefix[1])) || aiPrefix == "703" || aiPrefix == "723")
+		using namespace std::literals;
+		if ((aiPrefix[0] == '3' && Contains("1234569", aiPrefix[1])) || aiPrefix == "703"sv || aiPrefix == "723"sv)
 			return 4;
 		else
-			return Size(aiPrefix);
+			return strlen(aiPrefix);
 	}
 };
 
@@ -292,26 +294,20 @@ std::string HRIFromGS1(std::string_view gs1)
 	return res;
 }
 
-#if __cplusplus > 201703L
-std::ostream& operator<<(std::ostream& os, const char8_t* str)
-{
-	return os << reinterpret_cast<const char*>(str);
-}
-#endif
-
 std::string HRIFromISO15434(std::string_view str)
 {
 	// Use available unicode symbols to simulate sub- and superscript letters as specified in
 	// ISO/IEC 15434:2019(E) 6. Human readable representation
 
-	std::ostringstream oss;
+	std::string res;
+	res.reserve(str.size());
 
 	for (char c : str) {
 #if 1
 		if (0 <= c && c <= 0x20)
-			oss << "\xe2\x90" << char(0x80 + c); // Unicode Block “Control Pictures”: 0x2400
+			(res += "\xe2\x90") += char(0x80 + c); // Unicode Block “Control Pictures”: 0x2400
 		else
-			oss << c;
+			res += c;
 #else
 		switch (c) {
 		case 4: oss << u8"\u1d31\u1d52\u209c"; break; // EOT
@@ -324,7 +320,7 @@ std::string HRIFromISO15434(std::string_view str)
 #endif
 	}
 
-	return oss.str();
+	return res;
 }
 
 } // namespace ZXing
