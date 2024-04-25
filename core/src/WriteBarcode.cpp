050fed2ac393615beb78164323f0938345063087
axxel <awagger@gmail.com>
2024-04-25 09:32:17 +0200

    WriteBarcode: fix copy'n'paste bug in WriteBarcodeToUtf8()

--- core/src/WriteBarcode.cpp
+++ core/src/WriteBarcode.cpp
@@ -475,7 +475,7 @@ std::string WriteBarcodeToUtf8(const Barcode& barcode, [[maybe_unused]] const Wr
 	for (int y = 0; y < iv.height(); y += 2) {
 		for (int x = 0; x < iv.width(); ++x) {
 			int tp = bool(*iv.data(x, y)) ^ inverted;
-			int bt = (iv.height() == 1 && tp) || (y + 1 < iv.height() && (bool(*iv.data(x, y)) ^ inverted));
+			int bt = (iv.height() == 1 && tp) || (y + 1 < iv.height() && (bool(*iv.data(x, y + 1)) ^ inverted));
 			res << map[tp | (bt << 1)];
 		}
 		res << '\n';
