zxing-cpp/zxing-cpp

commit e3672fdf329718f87ffd5431eb232937911d507f

ITF: reimplement start and stop pattern check based on reference algo
This fixes #775 properly in the sense that with a standard quiet zone of
10x in none of the 3 samples a wrong result is returned.

core/src/oned/ODITFReader.cpp
@@ -13,28 +13,33 @@
 
 namespace ZXing::OneD {
 
-constexpr auto START_PATTERN_ = FixedPattern<4, 4>{1, 1, 1, 1};
-constexpr auto STOP_PATTERN_1 = FixedPattern<3, 4>{2, 1, 1};
-constexpr auto STOP_PATTERN_2 = FixedPattern<3, 5>{3, 1, 1};
-
 Barcode ITFReader::decodePattern(int rowNumber, PatternView& next, std::unique_ptr<DecodingState>&) const
 {
 	const int minCharCount = 6;
 	const int minQuietZone = 6; // spec requires 10
 
-	next = FindLeftGuard(next, 4 + minCharCount/2 + 3, START_PATTERN_, minQuietZone);
+	next = FindLeftGuard(next, 4 + minCharCount/2 + 3, FixedPattern<4, 4>{1, 1, 1, 1}, minQuietZone);
 	if (!next.isValid())
 		return {};
 
-	std::string txt;
-	txt.reserve(20);
+	// get threshold of first character pair
+	auto threshold = NarrowWideThreshold(next.subView(4, 10));
+	if (!threshold.isValid())
+		return {};
+	// check that each bar/space in the start pattern is < threshold
+	for (int i = 0; i < 4; ++i)
+		if (next[i] > threshold[i])
+			return {};
 
 	constexpr int weights[] = {1, 2, 4, 7, 0};
 	int xStart = next.pixelsInFront();
 	next = next.subView(4, 10);
 
+	std::string txt;
+	txt.reserve(20);
+
 	while (next.isValid()) {
-		const auto threshold = NarrowWideThreshold(next);
+		threshold = NarrowWideThreshold(next);
 		if (!threshold.isValid())
 			break;
 
@@ -60,7 +65,12 @@ Barcode ITFReader::decodePattern(int rowNumber, PatternView& next, std::unique_p
 	if (Size(txt) < minCharCount || !next.isValid())
 		return {};
 
-	if (!IsRightGuard(next, STOP_PATTERN_1, minQuietZone) && !IsRightGuard(next, STOP_PATTERN_2, minQuietZone))
+	// Check quiet zone size
+	if (!(next.isAtLastBar() || next[3] > minQuietZone * (threshold.bar + threshold.space) / 3))
+		return {};
+
+	// Check stop pattern
+	if (next[0] < threshold[0] || next[1] > threshold[1] || next[2] > threshold[2])
 		return {};
 
 	Error error = _opts.validateITFCheckSum() && !GTIN::IsCheckDigitValid(txt) ? ChecksumError() : Error();

commit 8d9aecca78d1c37507d088fe150babf9655c5c8f

ITF: fix detection regression
introduced in e3672fdf329718f87ffd5431eb232937911d507f

Add test image that checks for support of slanted (non-perpendicular)
scanned symbols.

core/src/oned/ODITFReader.cpp
@@ -41,23 +41,26 @@ Barcode ITFReader::decodePattern(int rowNumber, PatternView& next, std::unique_p
 	txt.reserve(20);
 
 	while (next.isValid()) {
-		threshold = NarrowWideThreshold(next);
-		if (!threshold.isValid())
+		// look for end-of-symbol
+		if (next[3] > threshold.space * 3)
 			break;
 
 		BarAndSpace<int> digits, numWide;
+		bool bad = false;
 		for (int i = 0; i < 10; ++i) {
-			if (next[i] > threshold[i] * 2)
-				break;
+			bad |= next[i] > threshold[i] * 3 || next[i] < threshold[i] / 3;
 			numWide[i] += next[i] > threshold[i];
 			digits[i] += weights[i/2] * (next[i] > threshold[i]);
 		}
 
-		if (numWide.bar != 2 || numWide.space != 2)
+		if (bad || numWide.bar != 2 || numWide.space != 2)
 			break;
 
 		for (int i = 0; i < 2; ++i)
 			txt.push_back(ToDigit(digits[i] == 11 ? 0 : digits[i]));
+
+		// update threshold to support scanning slanted symbols (scanned non-perpendicular)
+		threshold = NarrowWideThreshold(next);
 
 		next.skipSymbol();
 	}
@@ -65,7 +68,8 @@ Barcode ITFReader::decodePattern(int rowNumber, PatternView& next, std::unique_p
 	next = next.subView(0, 3);
 
 	// Check quiet zone size
-	if (!next.isValid() || !(next.isAtLastBar() || next[3] > minQuietZone * (threshold.bar + threshold.space) / 3))
+	if (!next.isValid() || !threshold.isValid()
+		|| !(next.isAtLastBar() || next[3] > minQuietZone * (threshold.bar + threshold.space) / 3))
 		return {};
 
 	// Check stop pattern

test/blackbox/BlackboxTestRunner.cpp
@@ -464,9 +464,9 @@ int runBlackBoxTests(const fs::path& testPathPrefix, const std::set<std::string>
 			{ 3, 5, 180 },
 		}, ReaderOptions().setEanAddOnSymbol(EanAddOnSymbol::Require));
 
-		runTests("itf-1", "ITF", 12, {
-			{ 11, 12, 0   },
-			{ 11, 12, 180 },
+		runTests("itf-1", "ITF", 14, {
+			{ 13, 14, 0   },
+			{ 13, 14, 180 },
 		});
 
 		runTests("itf-2", "ITF", 6, {