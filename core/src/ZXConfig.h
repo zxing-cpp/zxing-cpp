dc74d7496395bafa9e0e11c78cd757c2137a8cec
Christian Schmitz <support@monkeybreadsoftware.de>
2025-01-10 14:44:29 +0100

    Added comment for thread_local

--- core/src/ZXConfig.h
+++ core/src/ZXConfig.h
@@ -9,6 +9,7 @@
 // in e.g. the ReedSolomonDecoder. It is disabled by default. It can be enabled by modifying the following define.
 // Note: The Apple clang compiler until XCode 8 does not support c++11's thread_local.
 // The alternative 'static' makes the code thread unsafe.
+// for Windows in Visual Studio 2019 on Intel 64-bit using thread_local causes a dependency to VCRUNTIME140_1.dll, so you need 2019 runtime DLLs instead of only 2015 version.
 #define ZX_THREAD_LOCAL thread_local // '' (nothing), 'thread_local' or 'static'
 
 // The Galoir Field abstractions used in Reed-Solomon error correction code can use more memory to eliminate a modulo
