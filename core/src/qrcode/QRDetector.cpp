a920817b6fe0508cc4aca9003003c2812a78e935
axxel <awagger@gmail.com>
2025-01-31 10:25:53 +0100

    QRDetector: require the central FP square to be at least 3 pixels
    This is considered a fix for #888.

--- core/src/qrcode/QRDetector.cpp
+++ core/src/qrcode/QRDetector.cpp
@@ -43,7 +43,7 @@ PatternView FindPattern(const PatternView& view)
 {
 	return FindLeftGuard<PATTERN.size()>(view, PATTERN.size(), [](const PatternView& view, int spaceInPixel) {
 		// perform a fast plausability test for 1:1:3:1:1 pattern
-		if (view[2] < 2 * std::max(view[0], view[4]) || view[2] < std::max(view[1], view[3]))
+		if (view[2] < 3 || view[2] < 2 * std::max(view[0], view[4]) || view[2] < std::max(view[1], view[3]))
 			return 0.;
 		return IsPattern<E2E>(view, PATTERN, spaceInPixel, 0.1); // the requires 4, here we accept almost 0
 	});
