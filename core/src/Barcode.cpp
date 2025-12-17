497708ad01060f2d02c9ec9fc621b45559a4fe6c
axxel <awagger@gmail.com>
2025-12-17 12:09:41 +0100

    Barcode: fix compile regression in ZXING_EXPERIMENTAL_API=OFF builds

--- core/src/Barcode.cpp
+++ core/src/Barcode.cpp
@@ -56,9 +56,11 @@ Result::Result(DecoderResult&& decodeResult, DetectorResult&& detectorResult, Ba
 	  , _json(std::move(decodeResult).json())
 #endif
 {
+#ifdef ZXING_EXPERIMENTAL_API
 	// the BitMatrix stores 'black'/foreground as 0xFF and 'white'/background as 0, but we
 	// want the ImageView returned by symbol() to be a standard luminance image (black == 0)
 	_symbol->flipAll();
+#endif
 
 	if (decodeResult.versionNumber())
 		snprintf(_version, 4, "%d", decodeResult.versionNumber());
