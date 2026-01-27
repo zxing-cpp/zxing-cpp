7a35815046f8ba5dd3f3a8c9b8d00d90b9e9b077
axxel <awagger@gmail.com>
2026-01-27 09:42:52 +0100

    qt: move qt-related code from examples to wrapper/qt
    * rename QtCamReader to QmlReader to make room for QWidget based example
    * rename ZXingQtReader.h to ZXingQt.h to prepare adding writer support

--- wrappers/qt/ZXingQmlReader.cpp
+++ wrappers/qt/ZXingQmlReader.cpp
@@ -6,7 +6,7 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
-#include "ZXingQtReader.h"
+#include "ZXingQt.h"
 
 int main(int argc, char *argv[])
 {
@@ -18,9 +18,9 @@ int main(int argc, char *argv[])
 	app.setApplicationName(QStringLiteral("ZXingQtCamReader"));
 	QQmlApplicationEngine engine;
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-	engine.load(QUrl(QStringLiteral("qrc:/ZXingQt5CamReader.qml")));
+	engine.load(QUrl(QStringLiteral("qrc:/ZXingQml5Reader.qml")));
 #else
-	engine.load(QUrl(QStringLiteral("qrc:/ZXingQt6CamReader.qml")));
+	engine.load(QUrl(QStringLiteral("qrc:/ZXingQml6Reader.qml")));
 #endif
 	if (engine.rootObjects().isEmpty())
 		return -1;
--- wrappers/qt/ZXingQtReader.cpp
+++ wrappers/qt/ZXingQtReader.cpp
@@ -3,7 +3,7 @@
 */
 // SPDX-License-Identifier: Apache-2.0
 
-#include "ZXingQtReader.h"
+#include "ZXingQt.h"
 
 #include <QDebug>
 
