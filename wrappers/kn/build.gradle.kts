41a380cc0f17be639528969d1140c7fbfabfe506
axxel <awagger@gmail.com>
2026-02-12 00:20:33 +0100

    kn: fix version string (remove "-SNAPSHOT")
    This is possibly related to
    https://github.com/zxing-cpp/zxing-cpp/pull/1055#issuecomment-3886199881

--- wrappers/kn/build.gradle.kts
+++ wrappers/kn/build.gradle.kts
@@ -19,7 +19,7 @@ plugins {
 }
 
 group = "io.github.zxing-cpp"
-version = "3.0.0-SNAPSHOT"
+version = "3.0.0"
 
 Properties().apply {
     rootProject.file("local.properties").takeIf { it.exists() && it.isFile }?.let { load(it.reader()) }
