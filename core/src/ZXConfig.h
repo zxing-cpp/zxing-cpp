463fa528046afcf72ba4406043438939df67f663
axxel <awagger@gmail.com>
2026-03-09 09:42:43 +0100

    ZXConfig: udpate comment about THREAD_LOCAL

--- core/src/ZXConfig.h
+++ core/src/ZXConfig.h
@@ -6,10 +6,10 @@
 #pragma once
 
 // Thread local or static memory may be used to reduce the number of (re-)allocations of temporary variables
-// in e.g. the ReedSolomonDecoder. It is disabled by default. It can be enabled by modifying the following define.
-// Note: The Apple clang compiler until XCode 8 does not support c++11's thread_local.
-// The alternative 'static' makes the code thread unsafe.
-// for Windows in Visual Studio 2019 on Intel 64-bit using thread_local causes a dependency to VCRUNTIME140_1.dll, so you need 2019 runtime DLLs instead of only 2015 version.
+// in e.g. the ReedSolomonDecoder. The default is thread_local, which is the best option for performance and safety in most cases. If
+// your platform doesn't support thread_local, you can switch to static, but be aware that this makes the code not thread safe.
+// For Windows in Visual Studio 2019 on Intel 64-bit using thread_local causes a dependency to VCRUNTIME140_1.dll, so you need 2019
+// runtime DLLs instead of only 2015 version.
 #define ZX_THREAD_LOCAL thread_local // '' (nothing), 'thread_local' or 'static'
 
 // The Galoir Field abstractions used in Reed-Solomon error correction code can use more memory to eliminate a modulo
