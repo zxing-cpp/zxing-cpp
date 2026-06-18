8b7239aa6d21b69765e60262da3f3c82a8bca3dc
axxel <awagger@gmail.com>
2026-06-18 15:45:15 +0200

    Telepen: add support for alternative START/STOP patterns from AIM spec

--- core/src/oned/ODTelepenReader.cpp
+++ core/src/oned/ODTelepenReader.cpp
@@ -16,28 +16,24 @@
 #define printf(...){}
 #endif
 
+// This code is based on the AIM Europe USS Telepen (1991) specification and ISO/IEC 15424:2025.
+// See also https://advanova.co.uk/wp-content/uploads/2022/05/Barcode-Symbology-information-and-History.pdf
+
 namespace ZXing::OneD {
 
 static std::string DecodeNumeric(std::string_view encoded)
 {
 	std::string decoded;
 	decoded.reserve(encoded.size() * 2);
-	bool inAlphaTail = false;
 
 	for (uint8_t codeword : encoded) {
-		// 16 is the "shift to alpha tail" codeword,
-		// see https://advanova.co.uk/wp-content/uploads/2022/05/Barcode-Symbology-information-and-History.pdf
-		if (!inAlphaTail && codeword == 16)
-			inAlphaTail = true;
-		else if (inAlphaTail)
-			decoded += codeword;
-		else if (17 <= codeword && codeword < 27) {
-			decoded += ToDigit(codeword - 17);
-			decoded += 'X';
-		}
+		if (codeword < 16 || codeword == 127)
+			decoded += codeword; // control characters
+		else if (17 <= codeword && codeword < 27)
+			(decoded += ToDigit(codeword - 17)) += 'X';
 		else if (27 <= codeword && codeword < 127)
 			decoded += ToString(codeword - 27, 2);
-		else
+		else // (codeword == 16): 16/DLE is the "shift to alpha" codeword and can't appear twice
 			return {};
 	}
 
@@ -49,8 +45,20 @@ BarcodeData TelepenReader::decodePattern(int rowNumber, PatternView& next, std::
 	constexpr int minCharCount = 1; // TODO
 	constexpr int minQuietZone = 5; // spec requires 10
 	constexpr int minCharLength = 16 / 3;
-	constexpr auto startPattern = FixedPattern<12, 16>{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3};
-	constexpr auto endPattern = FixedPattern<11, 15>{3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+	constexpr FixedPattern<12, 16> startPatterns[3] = {
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3}, // START 1: "Full ASCII" in AIM Spec
+		{1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 3}, // START 2: "Compressed Numeric (+ Full ASCII)" in AIM Spec
+		{1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 3}  // START 3: "Full ASCII + Compressed Numeric" in AIM Spec
+	};
+	constexpr FixedPattern<6, 6> prefixPattern = {1, 1, 1, 1, 1, 1};
+	constexpr FixedPattern<11, 15> endPatterns[3] = {
+		{3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1}, // STOP 1
+		{3, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1}, // STOP 2
+		{3, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1}  // STOP 3
+	};
+
+	// Note: In the real-world there are symbols with START 1 + STOP 1 patterns that may contain "Compressed Numeric (+ Full ASCII)"
+	// data, see guess-work below.
 
 #if 0 // use fast 1:1:1:1 start pattern plausibility check, then E2E check for the whole start pattern
 	next = FindLeftGuard<12>(next, 2 * 12 + minCharCount * minCharLength, [=](const PatternView& view, int spaceInPixel) {
@@ -64,12 +72,23 @@ BarcodeData TelepenReader::decodePattern(int rowNumber, PatternView& next, std::
 			   && IsPattern<true>(view, startPattern, spaceInPixel, minQuietZone);
 	});
 #else
-	next = FindLeftGuard<true>(next, 2 * 12 + minCharCount * minCharLength, startPattern, minQuietZone);
+	next = FindLeftGuard<true>(next, 2 * 12 + minCharCount * minCharLength, prefixPattern, minQuietZone);
 #endif
 	if (!next.isValid())
 		return {};
 
+	int startChar = 1;
+	for (; startChar < 4; ++startChar)
+		if (IsPattern<true>(next, startPatterns[startChar - 1], next.spaceInFront(), minQuietZone))
+			break;
+	if (startChar == 4 || (startChar > 1 && !readNumeric))
+		return {};
+
+	const auto &startPattern = startPatterns[startChar - 1];
+	const auto &endPattern = endPatterns[startChar - 1];
+
 	int xStart = next.pixelsInFront();
+	next = next.subView(0, startPattern.size());
 
 	// get threshold from start pattern
 	auto threshold = NarrowWideThreshold(next);
@@ -128,22 +147,50 @@ BarcodeData TelepenReader::decodePattern(int rowNumber, PatternView& next, std::
 
 	auto txt = raw.substr(0, raw.size() - 1); // drop checksum character
 	auto checkSum = (127 - (Reduce(txt, 0) % 127)) % 127;
-	Error error = checkSum != raw.back() ? Error::Checksum : Error{};
+	char modifier = 0;
+	Error error;
 
-	SymbologyIdentifier symbologyIdentifier = {'B', '0'};
-
-	if (readNumeric && (!readAlpha || std::ranges::any_of(txt, [](uint8_t c) { return c < 32; }))) {
-		if (auto decoded = DecodeNumeric(txt); !decoded.empty()) {
-			// see ISO/IEC 15424:2025 4.4.3
-			symbologyIdentifier.modifier = std::ranges::any_of(txt, [](uint8_t c) { return c == 16; }) ? '2' : '1';
-			txt = std::move(decoded);
+	if (checkSum != raw.back()) {
+		error = Error::Checksum;
+	} else {
+		if (startChar == 1 && readNumeric) { // "Full ASCII" in AIM Spec or "Compressed Numeric (+ Full ASCII)" in "the wild"
+			// Apply guesswork to determine whether to treat as "Full ASCII" or "Compressed Numeric (+ Full ASCII)"
+			// Num+Alpha (startChar is 2) is likely if there is at most one DLE that is not the first character and there are
+			// non-printable ASCII characters but none that are not used in "Compressed Numeric" to encode digits.
+			if (!readAlpha
+				|| (std::ranges::count(txt, '\x10') <= 1 && txt.front() != '\x10'
+					&& std::ranges::any_of(txt, [](uint8_t c) { return c < 32; })
+					&& std::ranges::none_of(txt, [](uint8_t c) { return c < 16; })))
+				startChar = 2;
 		}
-		else if (!readAlpha)
-			return {};
+
+		const auto dle = IndexOf(txt, '\x10'); // DLE (== 16) is the "shift" codeword for switching between alpha and numeric modes
+		// see ISO/IEC 15424:2025 4.4.3
+		if (startChar == 1) {
+			modifier = '0'; // Full ASCII mode
+		} else if (startChar == 2 && dle != 0) {
+			auto num = dle == -1 ? DecodeNumeric(txt) : DecodeNumeric(txt.substr(0, dle));
+			auto alpha = dle == -1 ? std::string() : txt.substr(dle + 1);
+			if (num.empty())
+				error = Error::Format; // invalid numeric encoding
+			else
+				txt = num + alpha;
+			modifier = dle == -1 ? '1' : '2'; // Double density numeric: only (1) / followed by full ASCII (2)
+		} else if (startChar == 3 && dle != 0) {
+			auto alpha = dle == -1 ? txt : txt.substr(0, dle);
+			auto num = dle == -1 ? std::string() : DecodeNumeric(txt.substr(dle + 1));
+			if (dle != -1 && num.empty())
+				error = Error::Format; // invalid numeric encoding
+			else
+				txt = alpha + num;
+			modifier = dle == -1 ? '0' : '4'; // Full ASCII: only (0) / followed by double density numeric (4)
+		} else
+			error = Error::Format; // DLE as first character is not allowed
 	}
 
 	int xStop = next.pixelsTillEnd();
-	auto format = symbologyIdentifier.modifier == '1' ? BarcodeFormat::TelepenNumeric : BarcodeFormat::TelepenAlpha;
+	SymbologyIdentifier symbologyIdentifier = {'B', modifier};
+	auto format = modifier == '1' ? BarcodeFormat::TelepenNumeric : BarcodeFormat::TelepenAlpha;
 	printf("line: %d, raw: %s, txt: %s, checksum: %d\n", rowNumber, raw.c_str(), txt.c_str(), checkSum);
 
 	return LinearBarcode(format, txt, rowNumber, xStart, xStop, symbologyIdentifier, error);
--- test/unit/oned/ODTelepenReaderTest.cpp
+++ test/unit/oned/ODTelepenReaderTest.cpp
@@ -10,13 +10,25 @@
 using namespace ZXing;
 using namespace ZXing::OneD;
 
-static Barcode Parse(PatternRow row, std::initializer_list<ZXing::PatternType> tail = {0})
+static Barcode Parse(int start, PatternRow row, std::initializer_list<ZXing::PatternType> tail = {0},
+					 BarcodeFormat format = BarcodeFormat::Telepen)
 {
-	auto opts = ReaderOptions().formats(BarcodeFormat::Telepen);
+	auto opts = ReaderOptions().formats(format);
 	TelepenReader reader(opts);
 
-	row.insert(row.begin(), { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3 });
-	row.insert(row.end(), { 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1});
+	static const std::initializer_list<ZXing::PatternType> starts[3] = {
+		{ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3 },
+		{ 0, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 3 },
+		{ 0, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 3 }
+	};
+	static const std::initializer_list<ZXing::PatternType> stops[3] = {
+		{ 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 3, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1 },
+		{ 3, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1 }
+	};
+
+	row.insert(row.begin(), starts[start - 1]);
+	row.insert(row.end(), stops[start - 1]);
 	row.insert(row.end(), tail);
 
 	PatternView next(row);
@@ -27,7 +39,7 @@ static Barcode Parse(PatternRow row, std::initializer_list<ZXing::PatternType> t
 TEST(ODTelepenReaderTest, Alpha)
 {
 	{
-		auto result = Parse(PatternRow({
+		auto result = Parse(1, PatternRow({
 			1, 1, 3, 1, 3, 1, 3, 3,            // A
 			1, 3, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1 // checksum
 		}));
@@ -38,24 +50,24 @@ TEST(ODTelepenReaderTest, Alpha)
 	}
 
 	{
-		auto result = Parse(PatternRow({1, 1, 3, 1, 3, 1, 3, 3, 1, 3, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1}), {1});
+		auto result = Parse(1, PatternRow({1, 1, 3, 1, 3, 1, 3, 3, 1, 3, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1}), {1});
 		EXPECT_EQ(result.text(), "A");
 	}
 
 	{
-		auto result = Parse(PatternRow({1, 1, 3, 1, 3, 1, 3, 3, 1, 3, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1}), {100});
+		auto result = Parse(1, PatternRow({1, 1, 3, 1, 3, 1, 3, 3, 1, 3, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1}), {100});
 		EXPECT_EQ(result.text(), "A");
 	}
 
 	{
-		auto result = Parse(PatternRow({1, 1, 3, 1, 3, 1, 3, 3, 1, 3, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1}), {100, 5});
+		auto result = Parse(1, PatternRow({1, 1, 3, 1, 3, 1, 3, 3, 1, 3, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1}), {100, 5});
 		EXPECT_EQ(result.text(), "A");
 	}
 }
 
 TEST(ODTelepenReaderTest, Numeric)
 {
-	auto result = Parse(PatternRow({
+	auto result = Parse(1, PatternRow({
 		1, 1, 3, 1, 1, 1, 3, 1, 1, 1, 1, 1,
 		1, 1, 1, 1, 1, 1, 3, 3, 3, 1,
 		3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1,
@@ -67,9 +79,68 @@ TEST(ODTelepenReaderTest, Numeric)
 	EXPECT_EQ(result.text(), "466X33");
 }
 
+TEST(ODTelepenReaderTest, NumAlpha)
+{
+	auto result = Parse(1, PatternRow({
+		1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 3, 1, // 12
+		3, 1, 3, 1, 1, 1, 3, 1, 1, 1,       // <DLE>
+		1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 3, 1, // 3
+		1, 1, 3, 3, 1, 1, 3, 1, 1, 1        // checksum 21
+	}));
+
+	ASSERT_TRUE(result.isValid());
+	// EXPECT_EQ(result.format(), BarcodeFormat::TelepenNumAlpha);
+	EXPECT_EQ(result.symbologyIdentifier(), "]B2");
+	EXPECT_EQ(result.text(), "123");
+}
+
+TEST(ODTelepenReaderTest, NumControl)
+{
+	auto result = Parse(1, PatternRow({
+		1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 3, 1, // 12
+		1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 3, 1, // <SI> (ASCII 15)
+		1, 1, 1, 3, 1, 1, 1, 1, 1, 3, 1, 1, // 34
+		3, 1, 1, 1, 1, 1, 3, 1, 3, 1,       // checksum 12
+	}), {0}, BarcodeFormat::TelepenNumeric);
+
+	ASSERT_TRUE(result.isValid());
+	// EXPECT_EQ(result.format(), BarcodeFormat::TelepenNumeric);
+	EXPECT_EQ(result.symbologyIdentifier(), "]B1");
+	EXPECT_EQ(result.text(), "12<SI>34");
+}
+
+TEST(ODTelepenReaderTest, AIMNumAlpha)
+{
+	auto result = Parse(2, PatternRow({
+		1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 3, 1, // 12
+		3, 1, 1, 3, 1, 3, 1, 1, 1, 1        // checksum (88)
+	}));
+
+	ASSERT_TRUE(result.isValid());
+	// EXPECT_EQ(result.format(), BarcodeFormat::TelepenNumAlpha);
+	EXPECT_EQ(result.symbologyIdentifier(), "]B1");
+	EXPECT_EQ(result.text(), "12");
+}
+
+
+TEST(ODTelepenReaderTest, AIMAlphaNum)
+{
+	auto result = Parse(3, PatternRow({
+		1, 1, 3, 1, 3, 1, 3, 3,             // A
+		3, 1, 3, 1, 1, 1, 3, 1, 1, 1,       // <DLE>
+		1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 3, 1, // 12
+		1, 1, 1, 1, 1, 1, 3, 1, 3, 1, 1, 1  // checksum (7)
+	}));
+
+	ASSERT_TRUE(result.isValid());
+	// EXPECT_EQ(result.format(), BarcodeFormat::TelepenAlphaNum);
+	EXPECT_EQ(result.symbologyIdentifier(), "]B4");
+	EXPECT_EQ(result.text(), "A12");
+}
+
 TEST(ODTelepenReaderTest, WrongChecksum)
 {
-	auto result = Parse(PatternRow({
+	auto result = Parse(1, PatternRow({
 		1, 1, 3, 1, 3, 1, 3, 3, // A
 		1, 1, 3, 1, 3, 1, 3, 3  // wrong checksum
 	}));
