4ae7f7ba891819ae554a947aea0ef4c0997adeab
axxel <awagger@gmail.com>
2026-06-23 10:50:28 +0200

    Barcode: fix property in extra("ALL") JSON output ("Type" -> "Format")
    This was an oversight/left over from the whole Format/Type/Symbology/
    Variant experimentation phase.

--- core/src/Barcode.cpp
+++ core/src/Barcode.cpp
@@ -151,7 +151,7 @@ std::string Barcode::extra(std::string_view key) const
 		auto res =
 			StrCat("{", JsonProp("Text", text(TextMode::Plain)), JsonProp("HRI", text(TextMode::HRI)),
 				   JsonProp("TextECI", text(TextMode::ECI)), JsonProp("Bytes", text(TextMode::Hex)),
-				   JsonProp("Identifier", symbologyIdentifier()), JsonProp("Type", Name(format())),
+				   JsonProp("Identifier", symbologyIdentifier()), JsonProp("Format", Name(format())),
 				   JsonProp("Symbology", Name(Symbology(format()))), JsonProp("ContentType", isValid() ? ToString(contentType()) : ""),
 				   JsonProp("Position", ToString(position())), JsonProp("HasECI", hasECI()), JsonProp("IsMirrored", isMirrored()),
 				   JsonProp("IsInverted", isInverted()), d->extra, JsonProp("Error", ToString(error())));
