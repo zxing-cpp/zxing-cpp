af93fe675e27ebd4dfba108fa5fdf3b89a9e85f1
axxel <awagger@gmail.com>
2026-09-08 13:18:25 +0200

    MergeStructuredAppendSequence: prevent merging across symbologies
    Though unlikely, the old code could potentially try to merge 2 sequences
    from 2 symbologies (that happened to have the same sequenceId).

--- core/src/Barcode.cpp
+++ core/src/Barcode.cpp
@@ -243,7 +243,7 @@ Barcodes MergeStructuredAppendSequences(const Barcodes& barcodes)
 	std::map<std::string, Barcodes> sas;
 	for (auto& barcode : barcodes) {
 		if (barcode.isPartOfSequence())
-			sas[barcode.sequenceId()].push_back(barcode);
+			sas[ToString(barcode.format()) + barcode.sequenceId()].push_back(barcode);
 	}
 
 	Barcodes res;
